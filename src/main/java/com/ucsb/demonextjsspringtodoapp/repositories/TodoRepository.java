7703f82a8ac96663dd4cb15f69d530d0c6f235d3
Scott Chow <tehchowster@gmail.com>
2020-03-27 13:31:57 -0700

    sc - update repository

--- src/main/java/com/ucsb/demonextjsspringtodoapp/repositories/TodoRepository.java
+++ src/main/java/com/ucsb/demonextjsspringtodoapp/repositories/TodoRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.ucsb.demonextjsspringtodoapp.models.Todo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
   List<Todo> findByUserId(String userId);
 }
