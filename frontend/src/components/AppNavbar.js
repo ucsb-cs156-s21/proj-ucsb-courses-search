b5562be0bd9f2f41dea6d913f1f3077105c5b804
Scott Chow <tehchowster@gmail.com>
2020-09-09 13:52:26 -0700

    sc - link auth0 log in to login button, add some nav links

--- frontend/src/components/AppNavbar.js
+++ frontend/src/components/AppNavbar.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
+import { Nav, Navbar } from "react-bootstrap";
+import LoginButton from "./LoginButton";
 
 function AppNavbar() {
   return (
     <Navbar>
       <Navbar.Brand href="#home">Demo Spring React App</Navbar.Brand>
+      <Nav>
+        <Nav.Link href="#home">Todos</Nav.Link>
+      </Nav>
+      <Navbar.Collapse className="justify-content-end">
+        <Nav.Link href="#profile">User Info</Nav.Link>
+        <LoginButton />
+      </Navbar.Collapse>
     </Navbar>
   );
 }
--- frontend/src/components/AppNavbar.test.js
+++ frontend/src/components/AppNavbar.test.js
@@ -2,8 +2,27 @@ import React from "react";
 import { render } from "@testing-library/react";
 import AppNavbar from "./AppNavbar";
 
-test("should render the correct brand text", () => {
-  const { getByText } = render(<AppNavbar />);
-  const brandElement = getByText(/Demo Spring React App/);
-  expect(brandElement).toBeInTheDocument();
+describe("AppNavbar tests", () => {
+  test("should render the correct brand text", () => {
+    const { getByText } = render(<AppNavbar />);
+    const brandElement = getByText(/Demo Spring React App/);
+    expect(brandElement).toBeInTheDocument();
+  });
+
+  test("should have the correct links in the navbar", () => {
+    const { getByText } = render(<AppNavbar />);
+
+    const todosLink = getByText(/Todos/);
+    expect(todosLink.href).toMatch(/#home/);
+
+    const userInfoLink = getByText(/User Info/);
+    expect(userInfoLink.href).toMatch(/#profile/);
+  });
+
+  test("should show log in button", () => {
+    const { getByText } = render(<AppNavbar />);
+
+    const loginButton = getByText(/Log In/);
+    expect(loginButton).toBeInTheDocument();
+  });
 });
