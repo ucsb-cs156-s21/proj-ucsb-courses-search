55fb3f56513f74766241d7015231d0901359ec3e
Scott Chow <tehchowster@gmail.com>
2020-09-12 12:45:24 -0700

    sc - add tests for react controller

--- src/main/java/com/ucsb/demonextjsspringtodoapp/config/SecurityConfig.java
+++ src/main/java/com/ucsb/demonextjsspringtodoapp/config/SecurityConfig.java
@@ -16,8 +16,7 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
-    http.cors((arg) -> {
-    }).authorizeRequests().mvcMatchers("/api/public").permitAll().mvcMatchers("/api/**").authenticated().anyRequest()
+    http.authorizeRequests().mvcMatchers("/api/public").permitAll().mvcMatchers("/api/**").authenticated().anyRequest()
         .permitAll();
   }
 
