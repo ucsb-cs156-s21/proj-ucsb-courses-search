1d7fe2ecf2075a890552227b576fc02f89bdf0ee
Scott Chow <tehchowster@gmail.com>
2020-09-16 15:35:30 -0700

    sc - properly rename folder with capitalization, move App files back to src

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-import Loading from "../components/Loading/Loading";
-// import AppNavbar from "../components/Nav/AppNavbar";
+import Loading from "components/Loading/Loading";
+import AppNavbar from "components/Nav/AppNavbar";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
-import AppFooter from "../components/Footer/AppFooter";
-import Home from "../pages/Home/Home";
-import Profile from "../pages/Profile/Profile";
-import PrivateRoute from "../components/Auth/PrivateRoute";
-import TodoList from "../pages/Todos/Todos";
+import AppFooter from "components/Footer/AppFooter";
+import Home from "pages/Home/Home";
+import Profile from "pages/Profile/Profile";
+import PrivateRoute from "components/Auth/PrivateRoute";
+import TodoList from "pages/Todos/Todos";
 
 function App() {
   const { isLoading } = useAuth0();
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <AppNavbar /> */}
+      <AppNavbar />
       <Container className="flex-grow-1 mt-5">
         <Switch>
           <Route path="/" exact component={Home} />
