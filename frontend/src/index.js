67eb821ce3abfdf77e9aa890e0da0c8d6990582a
Scott Chow <tehchowster@gmail.com>
2020-09-16 15:20:41 -0700

    sc - attempt to move App into a folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./app/App";
 import Auth0ProviderWithHistory from "components/Auth/Auth0ProviderWithHistory";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
