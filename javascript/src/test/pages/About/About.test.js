f9c52ccc3697429f82c6dc436e4ceb5d6310e29d
Tanay Komarlu <tkomarlu@ucsb.edu>
2020-10-19 21:34:00 -0700

    tk - fixed about endpoint tests

--- javascript/src/test/pages/About/About.test.js
+++ javascript/src/test/pages/About/About.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Home from "main/pages/About/About";
+import About from "main/pages/About/About";
 
 describe("About tests", () => {
   test("renders without crashing", () => {
-    render(<Home />);
+    render(<About />);
   });
 });
