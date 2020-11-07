24f1f7217c11a6a3f20a45e7c8962bc33b3fd0f9
andrewhlu <alu@ucsb.edu>
2020-11-06 19:06:38 -0800

    AL - Add tests for filter helper and dropdown

--- javascript/src/test/utils/todoHelpers.test.js
+++ javascript/src/test/utils/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import { sortTodos } from "main/utils/todoHelpers";
+import { sortTodos, filterTodo } from "main/utils/todoHelpers";
 
 describe("todo helper tests", () => {
   test("sortTodo sorts todos by completion status then by value", () => {
@@ -39,5 +39,59 @@ describe("todo helper tests", () => {
       },
     ];
     expect(sortTodos(initialOrder)).toEqual(expectedOrder);
+  });
+
+  test("filterTodo does not remove incomplete todo when filtering by All", () => {
+    const todo =  {
+      value: "a",
+      done: false,
+    };
+    const filter = "All";
+    expect(filterTodo(todo, filter)).toEqual(true);
+  });
+
+  test("filterTodo does not remove complete todo when filtering by All", () => {
+    const todo =  {
+      value: "a",
+      done: true,
+    };
+    const filter = "All";
+    expect(filterTodo(todo, filter)).toEqual(true);
+  });
+
+  test("filterTodo removes incomplete todo when filtering by Complete", () => {
+    const todo =  {
+      value: "a",
+      done: false,
+    };
+    const filter = "Complete";
+    expect(filterTodo(todo, filter)).toEqual(false);
+  });
+
+  test("filterTodo does not remove complete todo when filtering by Complete", () => {
+    const todo =  {
+      value: "a",
+      done: true,
+    };
+    const filter = "Complete";
+    expect(filterTodo(todo, filter)).toEqual(true);
+  });
+
+  test("filterTodo does not remove incomplete todo when filtering by Incomplete", () => {
+    const todo =  {
+      value: "a",
+      done: false,
+    };
+    const filter = "Incomplete";
+    expect(filterTodo(todo, filter)).toEqual(true);
+  });
+
+  test("filterTodo removes complete todo when filtering by Incomplete", () => {
+    const todo =  {
+      value: "a",
+      done: true,
+    };
+    const filter = "Incomplete";
+    expect(filterTodo(todo, filter)).toEqual(false);
   });
 });
