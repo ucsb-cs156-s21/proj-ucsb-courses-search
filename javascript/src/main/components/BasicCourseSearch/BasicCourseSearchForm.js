5866736ddf556ffa451c4c5bebdb0fc4d15ae815
maraDowning <mara.downing@comcast.net>
2020-11-11 12:54:51 -0800

    md/pc - created form for course search

---
+++ javascript/src/main/components/BasicCourseSearch/BasicCourseSearchForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Form,Button } from "react-bootstrap";
+
+const BasicCourseSearchForm = () => {
+  return (
+    <Form>
+        <Form.Group controlId="BasicSearch.Quarter">
+            <Form.Label>Quarter</Form.Label>
+            <Form.Control as="select">
+            <option>W21</option>
+            <option>F20</option>
+            </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="BasicSearch.Department">
+            <Form.Label>Department</Form.Label>
+            <Form.Control as="select">
+            <option>CMPSC</option>
+            <option>MATH</option>
+            </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="BasicSearch.CourseLevel">
+            <Form.Label>Course Level</Form.Label>
+            <Form.Control as="select">
+            <option value="L">Undergrad-Lower Division</option>
+            <option value="S">Undergrad-Upper Division</option>
+            <option value="U">Undergrad-All</option>
+            <option value="G">Graduate</option>
+            </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+        Submit
+        </Button>
+    </Form>
+  );
+};
+
+export default BasicCourseSearchForm;
--- javascript/src/main/pages/Home/Home.js
+++ javascript/src/main/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Jumbotron } from "react-bootstrap";
 import {useAuth0} from "@auth0/auth0-react";
 import { Redirect } from "react-router-dom";
+import BasicCourseSearchForm from "../../components/BasicCourseSearch/BasicCourseSearchForm";
 
 const Home = () => {
     const { isAuthenticated } = useAuth0();
@@ -10,10 +11,7 @@ const Home = () => {
             <Jumbotron>
                 <div className="text-left">
                     <h5>Welcome to the UCSB Courses Search App!</h5>
-                    <p>A form to enter quarter, department and level, 
-                        together with a "Search" button will appear here
-                        in an upcoming release.
-                    </p>
+                    <BasicCourseSearchForm />
                 </div>
             </Jumbotron>
     );
