8742d58ad92895b5abc5b75374f0ecfb9c23411c
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-11-12 14:34:44 -0800

    pc - implement curriculum service to retrieve UCSB Course data

--- src/main/java/edu/ucsb/courses/controllers/BasicSearchController.java
+++ src/main/java/edu/ucsb/courses/controllers/BasicSearchController.java
@@ -9,12 +9,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.ucsb.courses.services.UCSBCurriculumService;
 
 @RestController
 @RequestMapping("/api/public")
@@ -23,17 +26,23 @@ public class BasicSearchController {
 
     private ObjectMapper mapper = new ObjectMapper();
 
+    @Autowired
+    UCSBCurriculumService ucsbCurriculumService;
+
     @GetMapping("/basicsearch")
-    public ResponseEntity<String> basicsearch(@RequestParam String qtr, @RequestParam String dept,
-            @RequestParam String level) throws JsonProcessingException {
+    public ResponseEntity<String> basicsearch(
+        @RequestParam String qtr, 
+        @RequestParam String dept,
+        @RequestParam String level) 
+        throws JsonProcessingException {
 
-        Map<String,String> dummyDataMap = new HashMap<>();
-        dummyDataMap.put("qtr",qtr);
-        dummyDataMap.put("dept",dept);
-        dummyDataMap.put("level",level);
-        String dummyJSONData = mapper.writeValueAsString(dummyDataMap);
+        // Map<String,String> dummyDataMap = new HashMap<>();
+        // dummyDataMap.put("qtr",qtr);
+        // dummyDataMap.put("dept",dept);
+        // dummyDataMap.put("level",level);
+        // String dummyJSONData = mapper.writeValueAsString(dummyDataMap);
 
-        String body = dummyJSONData; // eventually replace with call to get real data
+        String body = ucsbCurriculumService.getJSON(dept, qtr, level);
 
         return ResponseEntity.ok().body(body);
     }
