8a0e5f0c5607dd114688ffe73d5d50dbd17d74c2
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-11-12 17:57:58 -0800

    pc - add more tests to increase test coverage
    Before, we were not testing passing A as the level, which takes
    a different code path.  We also were not testing what happens
    when the call to the API fails.  Now we test both of those.

--- src/test/java/edu/ucsb/courses/services/UCSBCurriculumServiceTest.java
+++ src/test/java/edu/ucsb/courses/services/UCSBCurriculumServiceTest.java
@@ -14,32 +14,53 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.web.client.HttpClientErrorException;
 
 @ExtendWith(SpringExtension.class)
 public class UCSBCurriculumServiceTest {
 
     @Mock
     private RestTemplate restTemplate;
-  
+
     @InjectMocks
     private UCSBCurriculumService ucs;
-  
+
     @Test
-    public void test_getJSON() throws Exception {
+    public void test_getJSON_success() throws Exception {
         String expectedResult = "{expectedResult}";
 
-      when(restTemplate.exchange(any(String.class), eq(HttpMethod.GET), any(HttpEntity.class),
-          eq(String.class)))
-              .thenReturn(new ResponseEntity<String>(expectedResult, HttpStatus.OK));
-  
-      String subjectArea = "CMPSC";
-      String quarter="20201";
-      String level="L";
+        when(restTemplate.exchange(any(String.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
+                .thenReturn(new ResponseEntity<String>(expectedResult, HttpStatus.OK));
 
-      String result = ucs.getJSON(subjectArea,quarter,level);
+        String subjectArea = "CMPSC";
+        String quarter = "20201";
+        String level = "L";
 
-      assertEquals(expectedResult,result);
+        String result = ucs.getJSON(subjectArea, quarter, level);
+
+        assertEquals(expectedResult, result);
+
+        level = "A";
+        result = ucs.getJSON(subjectArea, quarter, level);
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void test_getJSON_exception() throws Exception {
+
+        String expectedResult = "{\"error\": \"401: Unauthorized\"}";
+
+        when(restTemplate.exchange(any(String.class), eq(HttpMethod.GET), any(HttpEntity.class), eq(String.class)))
+                .thenThrow(HttpClientErrorException.class);
+
+        String subjectArea = "CMPSC";
+        String quarter = "20201";
+        String level = "L";
+
+        String result = ucs.getJSON(subjectArea, quarter, level);
+
+        assertEquals(expectedResult, result);
     }
 
 }
