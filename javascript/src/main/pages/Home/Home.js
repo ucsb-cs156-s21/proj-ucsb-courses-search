ecb5667c86a746d263785d37c8786701f5be1c55
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-11-17 15:32:41 -0800

    pc/sc/class - Added BasicCourseTable component to illustrate adding a component
    This component currently only shows information for the top level
    lecture sections; the student will need to do some work to figure out
    how to show discussion sections as well.

--- javascript/src/main/pages/Home/Home.js
+++ javascript/src/main/pages/Home/Home.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { Jumbotron } from "react-bootstrap";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Redirect } from "react-router-dom";
 import BasicCourseSearchForm from "../../components/BasicCourseSearch/BasicCourseSearchForm";
+import BasicCourseTable from "../../components/BasicCourseSearch/BasicCourseTable";
 import JSONPrettyCard from "../../components/Utilities/JSONPrettyCard";
 
 const Home = () => {
-    
+
     // every function that starts with "use" is a hook
     // e.g. useState, useSWR, useAuth0
 
@@ -15,14 +14,21 @@ const Home = () => {
     // setCourseJSON is the setter
     // the parameter to useState is the initial value of the state
 
-    const [courseJSON, setCourseJSON] = useState('{"course" : "cs148"}');
+    const initialCourseJSON = {
+        "pageNumber": 1,
+        "pageSize": 1,
+        "total": 0,
+        "classes": []
+    };
 
-    // const courseJSON = '{"course" : "cs156"}';
+    const [courseJSON, setCourseJSON] = useState(initialCourseJSON);
+
     return (
         <Jumbotron>
             <div className="text-left">
                 <h5>Welcome to the UCSB Courses Search App!</h5>
                 <BasicCourseSearchForm setCourseJSON={setCourseJSON} />
+                <BasicCourseTable classes={courseJSON.classes} />
                 <JSONPrettyCard
                     expression={"courseJSON"}
                     value={courseJSON}
