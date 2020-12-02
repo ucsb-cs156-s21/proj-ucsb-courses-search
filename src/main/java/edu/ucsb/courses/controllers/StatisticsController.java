04f546d7a194e2a7d01be6ce35ad44c82de29c7f
Zachary Minshall <zminshall@umail.ucsb.edu>
2020-12-01 20:23:39 -0800

    zm - add submit form

--- src/main/java/edu/ucsb/courses/controllers/StatisticsController.java
+++ src/main/java/edu/ucsb/courses/controllers/StatisticsController.java
@@ -1,73 +1,39 @@
 package edu.ucsb.courses.controllers;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
-import static org.springframework.data.mongodb.core.aggregation.Aggregation.limit;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.match;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.sort;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.unwind;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
-import org.bson.Document;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.aggregation.GroupOperation;
-import org.springframework.data.mongodb.core.aggregation.LimitOperation;
 import org.springframework.data.mongodb.core.aggregation.MatchOperation;
 import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
 import org.springframework.data.mongodb.core.aggregation.SortOperation;
+import org.springframework.data.mongodb.core.aggregation.UnwindOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
-
-
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.ucsb.courses.documents.Course;
-import edu.ucsb.courses.documents.CoursePage;
-import edu.ucsb.courses.documents.statistics.QuarterDept;
-import edu.ucsb.courses.repositories.ArchivedCourseRepository;
-import edu.ucsb.courses.services.UCSBCurriculumService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.mongodb.client.model.Accumulators;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
-import com.mongodb.client.model.Sorts;
+import edu.ucsb.courses.documents.statistics.FullCourse;
+import edu.ucsb.courses.documents.statistics.QuarterDept;
 
 @RestController
 @RequestMapping("/api/public/statistics")
@@ -77,6 +43,26 @@ public class StatisticsController {
 
     @Autowired
     private MongoTemplate mongoTemplate;
+    
+    @GetMapping(value = "/courseCount", produces = "application/json")
+    public ResponseEntity<String> courseCount() 
+        throws JsonProcessingException, Exception {
+        
+        SortOperation sortByQuarterAndDeptCode = sort(Sort.by(Direction.DESC, "quarter")).and(Sort.by(Direction.ASC, "deptCode"));
+
+        GroupOperation groupByQuarterAndDeptCode = group("quarter","deptCode").count().as("courseCount");
+           
+        Aggregation aggregation = newAggregation(groupByQuarterAndDeptCode, sortByQuarterAndDeptCode);
+        
+        AggregationResults<QuarterDept> result = 
+            mongoTemplate.aggregate(aggregation, "courses", QuarterDept.class);
+          
+        List<QuarterDept> qds = result.getMappedResults();
+
+        logger.info("qds={}",qds);
+        String body = mapper.writeValueAsString(qds);
+        return ResponseEntity.ok().body(body);
+    }
 
     @GetMapping(value = "/fullCoursesByDept", produces = "application/json")
     public ResponseEntity<String> numFullCoursesByDept(
@@ -85,9 +71,25 @@ public class StatisticsController {
         @RequestParam String department)
         throws JsonProcessingException, Exception {
 
-            // Real aggregation needs to go here
+            MatchOperation filterQuarterDept = match(Criteria.where("quarter").gte(startQuarter).lte(endQuarter)
+                .and("deptCode").is(department).and("instructionType").is("LEC"));
+            UnwindOperation unwindOperation = unwind("$classSections","arrayIndex", false);
+            MatchOperation filterSection = match(Criteria.where("arrayIndex").ne(0));
+            GroupOperation groupOperation = group("$_id", "$quarter", "$title", "$courseId").sum("$classSections.enrolledTotal").as("enrolled")
+                .sum("$classSections.maxEnroll").as("maxEnrolled");
+            ProjectionOperation project = project("_id", "quarter", "title", "courseId", "enrolled", "maxEnrolled").andExpression("maxEnrolled - enrolled").as("diff");
+            MatchOperation filterFull = match(Criteria.where("diff").lte(0));
+            SortOperation sort = sort(Sort.by(Direction.ASC, "_id"));
 
-            String body = "{\"startQuarter\": \"" + startQuarter + "\"}";
+            Aggregation aggregation = newAggregation(filterQuarterDept, unwindOperation, filterSection, groupOperation, project, filterFull, sort);
+
+            AggregationResults<FullCourse> result = mongoTemplate.aggregate(aggregation, "courses", FullCourse.class);
+
+            List<FullCourse> fcs = result.getMappedResults();
+
+            logger.info("fcs={}",fcs);
+            String body = mapper.writeValueAsString(fcs);
+
             return ResponseEntity.ok().body(body);
     }
 }
--- src/test/java/edu/ucsb/courses/controllers/StatisticsControllerTests.java
+++ src/test/java/edu/ucsb/courses/controllers/StatisticsControllerTests.java
@@ -1,35 +1,30 @@
 package edu.ucsb.courses.controllers;
 
-import edu.ucsb.courses.config.SecurityConfig;
-import edu.ucsb.courses.documents.Course;
-import edu.ucsb.courses.documents.CoursePage;
-import edu.ucsb.courses.documents.statistics.QuarterDept;
-import edu.ucsb.courses.repositories.ArchivedCourseRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.bson.Document;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import edu.ucsb.courses.config.SecurityConfig;
+import edu.ucsb.courses.documents.Course;
+import edu.ucsb.courses.documents.statistics.FullCourse;
+import edu.ucsb.courses.documents.statistics.QuarterDept;
 
 // @Import(SecurityConfig.class) applies the security rules 
 // so that /api/public/** endpoints don't require authentication.
@@ -62,6 +57,36 @@ public class StatisticsControllerTests {
                 .andReturn();
         String responseString = response.getResponse().getContentAsString();
         List<QuarterDept> resultFromPage = QuarterDept.listFromJSON(responseString);
+
+        assertEquals(qdList, resultFromPage);
+    }
+
+    @Test
+    public void test_numFullCoursesByDept() throws Exception {
+        List<FullCourse> expectedResult = new ArrayList<FullCourse>();
+        String url = "/api/public/statistics/fullCoursesByDept";
+
+        org.bson.Document fakeRawResults = new org.bson.Document();
+        List<FullCourse> qdList = new ArrayList<FullCourse>();
+        qdList.add(new FullCourse("20204","SP TOP: GENERAL","CMPSC 190J"));
+        qdList.add(new FullCourse("20204","TRANS PROG LANG","CMPSC 160"));
+        qdList.add(new FullCourse("20204","ADV APP PROGRAM","CMPSC 156"));
+        qdList.add(new FullCourse("20204","INTRO TO COMP SCI","CMPSC 8"));
+        qdList.add(new FullCourse("20211","HUM-COMP INTERACT","CMPSC 185"));
+        qdList.add(new FullCourse("20211","COMPUTER GRAPHICS","CMPSC 180"));
+        qdList.add(new FullCourse("20211","DISTRD SYSTEMS","CMPSC 171"));
+        qdList.add(new FullCourse("20211","OPERATING SYSTEMS","CMPSC 170"));
+        qdList.add(new FullCourse("20211","COMPUTER ARCHITECT","CMPSC 154"));
+        AggregationResults<FullCourse> fakeResults = new AggregationResults<FullCourse>(qdList, fakeRawResults);
+
+        when(mongoTemplate.aggregate( any(Aggregation.class), eq("courses"), any(Class.class))).thenReturn(fakeResults);
+
+
+        MvcResult response = mockMvc.perform(get(url).queryParam("startQuarter", "20204").queryParam("endQuarter", "20211")
+            .queryParam("department", "CMPSC").contentType("application/json")).andExpect(status().isOk())
+            .andReturn();
+        String responseString = response.getResponse().getContentAsString();
+        List<FullCourse> resultFromPage = FullCourse.listFromJSON(responseString);
 
         assertEquals(qdList, resultFromPage);
     }
