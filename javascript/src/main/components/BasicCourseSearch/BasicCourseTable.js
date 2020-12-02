10f4d63cf27d22a18a682878ebccd1aefde8e076
Andrew Berry <andrewberry@umail.ucsb.edu>
2020-12-01 16:32:02 -0800

    AB/AZ - Reformatted JSON so it is a list of sections, Displayed all sections with lectures, added enroll code and section and instructor columns, made instructor column on display on lecture row

--- javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
+++ javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
@@ -3,21 +3,61 @@ import BootstrapTable from 'react-bootstrap-table-next';
 
 const BasicCourseTable = ( {classes} ) => {
   
+  const sections = [];
+
+  classes.forEach(
+    (course) => { 
+      course.classSections.forEach(
+        (section) => {
+          section.course = {
+          courseId: course.courseId,
+          title: course.title,
+          unitsFixed: course.unitsFixed
+          };
+         sections.push(section);
+       }
+      )
+    }
+  );
+
+const renderInstructors = (cell, row) => {
+  console.log(`cell=${cell} row=`, row);
+  const instructor = (row.instructors.length > 0)? row.instructors[0].instructor: "";
+        return (
+            <span>{instructor}</span>
+          )
+        }
+
   const columns = [{
-    dataField: 'courseId',
+    dataField: 'course.courseId',
     text: 'Course Number'
   },{
-    dataField: 'title',
+    dataField: 'course.title',
     text: 'Title'
   },{
-    dataField: 'unitsFixed',
+    dataField: 'enrollCode',
+    text: 'Enroll Code'
+  },{
+    dataField: 'course.unitsFixed',
     text: 'Unit'
-  }
+  },{
+    dataField: 'section',
+    text: 'Section'
+  },{
+            text: "Instructor",
+            isDummyField: true,
+            dataField: "instructors",
+            formatter: (cell, row) => renderInstructors(cell, row)
+        }
 ];
   
   return (
-    <BootstrapTable keyField='courseId' data={classes} columns={columns} />
+    <BootstrapTable keyField='enrollCode' data={sections} columns={columns} />
   );
 };
 
 export default BasicCourseTable;
+/*
+
+
+*/
