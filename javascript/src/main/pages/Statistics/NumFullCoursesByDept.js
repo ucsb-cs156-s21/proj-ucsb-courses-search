76f52283f9d7faa67efebd48741a6847950c4ace
Zachary Minshall <zminshall@umail.ucsb.edu>
2020-12-03 14:53:46 -0800

    zm - add bootstraptable

--- javascript/src/main/pages/Statistics/NumFullCoursesByDept.js
+++ javascript/src/main/pages/Statistics/NumFullCoursesByDept.js
@@ -2,23 +2,29 @@ import React from "react";
 import { useState } from "react";
 import { Jumbotron } from "react-bootstrap";
 import { fetchFullCourses } from "main/services/statisticsService";
-import JSONPrettyCard from "main/components/Utilities/JSONPrettyCard";
 import FullCoursesForm from "main/components/Statistics/FullCoursesForm";
+import FullCourseTable from "main/components/Statistics/FullCourseTable";
 
 const NumFullCoursesByDept = () => {
-    const [courseJSON, setCourseJSON] = useState('{"course" : "cs156"}');
+    const [tableVisibility, setTableVisibility] = useState(false);
+    const [tableData, setTableData] = useState([]);
 
+    const setJsonTableData = (json) => {
+        let newArray = json.map((item, index) => ({index, ...item}));
+        for (var i = 0; i < newArray.length; i++) {
+            newArray[i].index++;
+          }
+        setTableData(newArray);
+        setTableVisibility(true);
+    }
     return (
-        <Jumbotron> 
-        <div className="text-left">
-            <h5>Search Archived Course Data from MongoDB by Full Courses</h5>
-            <FullCoursesForm setCourseJSON={setCourseJSON} fetchJSON={fetchFullCourses} />
-            <JSONPrettyCard
-                expression={"courseJSON"}
-                value={courseJSON}
-            />
-        </div>
-    </Jumbotron>
+        <Jumbotron>
+            <div className="text-left">
+                <h5>Search Full Courses By Department</h5>
+                <FullCoursesForm setCourseJSON={setJsonTableData} fetchJSON={fetchFullCourses} />
+            </div>
+            {tableVisibility && (<FullCourseTable data={tableData} />)}
+        </Jumbotron>
     );
 };
 
