9c85ceec03fb2de2bf405b3868d00b5f73aa0121
jvoucsb <jvo@umail.ucsb.edu>
2020-12-04 00:10:04 -0800

    jv - add department and quarter form component  test

---
+++ javascript/src/test/components/Statistics/QuarterFormSelect.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import QuarterFormSelect from "main/components/Statistics/QuarterFormSelect";
+import userEvent from "@testing-library/user-event";
+
+describe("QuarterFormSelect tests", () => {
+  test("renders without crashing", () => {
+    render(<QuarterFormSelect />);
+  });
+
+  test("handle select works when I choose a quarter and then a year", () => {
+    const handleSelect = jest.fn();
+    const { getByTestId } = render(<QuarterFormSelect handleSelect={handleSelect} testId={"select"} />);
+    const selectQuarter = getByTestId("select-quarter")
+    userEvent.selectOptions(selectQuarter, "1");
+    const selectYear = getByTestId("select-year");
+    userEvent.selectOptions(selectYear, "2021");
+
+    expect(handleSelect).toHaveBeenCalledWith("20211");
+  });
+
+  test("handle select works when I choose year and then a quarter", () => {
+    const handleSelect = jest.fn();
+    const { getByTestId } = render(<QuarterFormSelect handleSelect={handleSelect} testId={"select"} />);
+    const selectYear = getByTestId("select-year");
+    userEvent.selectOptions(selectYear, "2021");
+    const selectQuarter = getByTestId("select-quarter")
+    userEvent.selectOptions(selectQuarter, "1");
+
+    expect(handleSelect).toHaveBeenCalledWith("20211");
+  });
+});
