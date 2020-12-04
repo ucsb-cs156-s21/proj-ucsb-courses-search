25725b5bbea12a64b8bec0cc13a58683f972742e
Mitch <mitchell5799@gmail.com>
2020-12-03 16:52:02 -0800

    ML - Added tests for ScheduleItem entity

--- src/main/java/edu/ucsb/courses/entities/ScheduleItem.java
+++ src/main/java/edu/ucsb/courses/entities/ScheduleItem.java
@@ -43,6 +43,17 @@ public class  ScheduleItem {
         this.userId = userId;
         this.schedule_id = schedule_id;
     }
+    
+    public ScheduleItem(){
+        this.id = 0L;
+        this.quarter = "null";
+        this.courseId = "null";
+        this.title = "null";
+        this.description = "null";
+        this.classSection = "null";
+        this.generalEducation = "null";
+        this.finalExam = "null";
+    }
 
 
     public Long getId() {
