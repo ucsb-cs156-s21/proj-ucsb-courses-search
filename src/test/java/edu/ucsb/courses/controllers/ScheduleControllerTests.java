ca747016b28a2820e53a3ec467fd8e60657fad0a
seanaocostello <sean.ao.costello@gmail.com>
2020-12-03 19:55:16 -0800

    Added getSchedule funtionality that get a schedule given a schedule id and tested it. Added stubs for create/edit/delete schedule functions in ScheduleController

---
+++ src/test/java/edu/ucsb/courses/controllers/ScheduleControllerTests.java
@@ -0,0 +1,104 @@
+package edu.ucsb.courses.controllers;
+
+import edu.ucsb.courses.config.SecurityConfig;
+import edu.ucsb.courses.repositories.ScheduleRepository;
+import edu.ucsb.courses.entities.Schedule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// @Import(SecurityConfig.class) applies the security rules 
+// so that /api/public/** endpoints don't require authentication.
+// Otherwise you may get authorization errors when running the test
+
+@WebMvcTest(value = ScheduleController.class)
+@Import(SecurityConfig.class)
+public class ScheduleControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ScheduleRepository scheduleRepository;
+
+    @Test
+    public void test_getScheduleSuccess() throws Exception {
+
+        String expectedResult = "Schedule[ id=1, name=CS 156, description=Adv App Programming, quarter=Fall 2020, userId=s ]";
+        String urlTemplate = "/api/public/getSchedule?id=%s";
+        String url = String.format(urlTemplate, "1");
+
+        Schedule schedule = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "s");
+
+        Optional<Schedule> opt = Optional.of(schedule);
+
+        when(scheduleRepository.findById(any(Long.class)))
+                .thenReturn(opt);
+
+        MvcResult response = mockMvc.perform(get(url).contentType("application/json")).andExpect(status().isOk())
+                .andReturn();
+
+        String responseString = response.getResponse().getContentAsString();
+
+        assertEquals(expectedResult, responseString);
+    }
+
+    @Test
+    public void test_getScheduleFailureNoSchedule() throws Exception {
+        String expectedResult = "";
+        String urlTemplate = "/api/public/getSchedule?id=%s";
+        String url = String.format(urlTemplate, "1");
+
+        //Schedule schedule = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "s");
+
+        Optional<Schedule> opt = Optional.empty();
+
+        when(scheduleRepository.findById(any(Long.class)))
+                .thenReturn(opt);
+
+        MvcResult response = mockMvc.perform(get(url).contentType("application/json")).andExpect(status().isBadRequest())
+                .andReturn();
+
+        String responseString = response.getResponse().getContentAsString();
+
+        assertEquals(expectedResult, responseString);
+    }
+
+    @Test
+    public void test_createScheduleFailure() throws Exception {
+        String expectedResult = "";
+        String urlTemplate = "/api/public/createSchedule?id=%s";
+        String url = String.format(urlTemplate, "1");
+
+        //Schedule schedule = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "s");
+
+        Optional<Schedule> opt = Optional.empty();
+
+        when(scheduleRepository.findById(any(Long.class)))
+                .thenReturn(opt);
+
+        MvcResult response = mockMvc.perform(get(url).contentType("application/json")).andExpect(status().isBadRequest())
+                .andReturn();
+
+        String responseString = response.getResponse().getContentAsString();
+
+        assertEquals(expectedResult, responseString);
+    }
+
+
+
+
+
+}
