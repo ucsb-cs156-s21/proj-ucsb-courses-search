d0aa335605efd4309ce08d10c02383c2cc025312
jvoucsb <jvo@umail.ucsb.edu>
2020-12-04 02:49:41 -0800

    jv - add test for table visibility in CourseOccupancy page for coverage

--- javascript/src/test/pages/Statistics/CourseOccupancy.test.js
+++ javascript/src/test/pages/Statistics/CourseOccupancy.test.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CourseOccupancy from "main/pages/Statistics/CourseOccupancy";
+import userEvent from "@testing-library/user-event";
+import { act } from 'react-dom/test-utils';
+
+import { fetchCourseOccupancy } from "main/services/statisticsService";
+jest.mock("main/services/statisticsService");
 
 describe("CourseOccupancy page tests", () => {
   test("renders without crashing", () => {
     render(<CourseOccupancy />);
+  });
+
+  test("CourseOccupancy table appears with data after pressing submit", async () => {
+    const { findByText } = render(<CourseOccupancy />);
+    const sampleReturnValue = [{
+      "quarter": "20211",
+      "enrolled": "100",
+      "maxEnrolled": "200"
+    }];
+
+    fetchCourseOccupancy.mockResolvedValue(sampleReturnValue);
+
+    const submitButton = await findByText("Submit");
+    userEvent.click(submitButton);
+
+    await findByText("WINTER 2021");
   });
 });
