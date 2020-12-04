e9fa5663da70ec0d6518b9da02d8077749da1f0d
jvoucsb <jvo@umail.ucsb.edu>
2020-12-04 01:48:08 -0800

    jv - convert quarter value to human readable for table data

--- javascript/src/main/components/Statistics/CourseOccupancyTable.js
+++ javascript/src/main/components/Statistics/CourseOccupancyTable.js
@@ -1,20 +1,20 @@
 import React from "react";
 import BootstrapTable from 'react-bootstrap-table-next';
 
-const CourseOccupancyTable = ( {data} ) => {
-  
+const CourseOccupancyTable = ({ data }) => {
+
   const columns = [{
     dataField: 'quarter',
     text: 'Quarter'
-  },{
+  }, {
     dataField: 'enrolled',
     text: '# Enrolled'
-  },{
+  }, {
     dataField: 'maxEnrolled',
     text: '# of Max Enrolled'
   }
-];
-  
+  ];
+
   return (
     <BootstrapTable keyField='quarter' data={data} columns={columns} />
   );
--- javascript/src/main/pages/Statistics/CourseOccupancy.js
+++ javascript/src/main/pages/Statistics/CourseOccupancy.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, Container } from "react-bootstrap";
 import { fetchCourseOccupancy } from "main/services/statisticsService";
 import CourseOccupancyForm from "main/components/Statistics/CourseOccupancyForm";
 import CourseOccupancyTable from "main/components/Statistics/CourseOccupancyTable";
+import { fromFormat } from "main/components/Statistics/QuarterFormSelect";
 
 const CourseOccupancy = () => {
     const [tableVisibility, setTableVisibility] = useState(false);
     const [tableData, setTableData] = useState([]);
 
     const setJsonTableData = (json) => {
+        json.forEach((item) => {
+            item["quarter"] = fromFormat(item["quarter"]);
+        });
         setTableData(json);
         setTableVisibility(true);
     }
 
     return (
         <Jumbotron>
-            <div className="text-left">
-                <h5>Course Occupancy by Department</h5>
+            <Container className="text-left">
+                <h1>Course Occupancy by Department</h1>
                 <CourseOccupancyForm setOccupancyJson={setJsonTableData} fetchJSON={fetchCourseOccupancy} />
-            </div>
-            {tableVisibility && (<CourseOccupancyTable data={tableData} />)}
+            </Container>
+            <Container>
+                {tableVisibility && (<CourseOccupancyTable data={tableData} />)}
+            </Container>
         </Jumbotron>
     );
 };
