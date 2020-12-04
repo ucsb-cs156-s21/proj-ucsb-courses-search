4b218b787c506636576deca953e6f61dcdda711b
Andy Ho <andyho@ucsb.edu>
2020-12-03 16:04:03 -0800

    ah - Added ScheduleRepository class to enable crud operations on schedule objects

---
+++ src/main/java/edu/ucsb/courses/repositories/ScheduleRepository.java
@@ -0,0 +1,13 @@
+package edu.ucsb.courses.repositories;
+
+import java.util.List;
+
+import edu.ucsb.courses.entities.Schedule;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+  List<Schedule> findAll();
+}
