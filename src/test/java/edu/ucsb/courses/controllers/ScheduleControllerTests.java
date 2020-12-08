ec28447386a3e2f488901797512d49bcc94cc6e3
Andy Ho <andyho@ucsb.edu>
2020-12-08 00:26:58 -0800

    ah - Added testing suite for the schedule controller class

--- src/test/java/edu/ucsb/courses/controllers/ScheduleControllerTests.java
+++ src/test/java/edu/ucsb/courses/controllers/ScheduleControllerTests.java
@@ -1,122 +1,140 @@
 package edu.ucsb.courses.controllers;
 
-import edu.ucsb.courses.config.SecurityConfig;
-import edu.ucsb.courses.repositories.ScheduleRepository;
-import edu.ucsb.courses.entities.Schedule;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// @Import(SecurityConfig.class) applies the security rules 
-// so that /api/public/** endpoints don't require authentication.
-// Otherwise you may get authorization errors when running the test
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ucsb.courses.repositories.ScheduleRepository;
+import edu.ucsb.courses.entities.Schedule;
 
 @WebMvcTest(value = ScheduleController.class)
-@Import(SecurityConfig.class)
+@WithMockUser
 public class ScheduleControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @MockBean
-    private ScheduleRepository scheduleRepository;
+  @Autowired
+  private ObjectMapper objectMapper;
 
-    @Test
-    public void test_getScheduleSuccess() throws Exception {
+  @MockBean
+  ScheduleRepository mockScheduleRepository;
 
-        String expectedResult = "Schedule[ id=1, name=CS 156, description=Adv App Programming, quarter=Fall 2020, userId=s ]";
-        String urlTemplate = "/api/public/getSchedule?id=%s";
-        String url = String.format(urlTemplate, "1");
+  private String userToken() {
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTYiLCJuYW1lIjoiSm9obiBEb2UiLCJpYXQiOjE1MTYyMzkwMjJ9.MkiS50WhvOFwrwxQzd5Kp3VzkQUZhvex3kQv-CLeS3M";
+  }
 
-        Schedule schedule = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "s");
+  @Test
+  public void testGetSchedule() throws Exception {
+    Schedule s1 = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "123456");
+    Optional<Schedule> expectedSchedules = Optional.of(s1);
 
-        Optional<Schedule> opt = Optional.of(schedule);
+    when(mockScheduleRepository.findById(1L)).thenReturn(expectedSchedules);
+    MvcResult response =
+        mockMvc
+            .perform(get("/api/public/getSchedule?id=1").contentType("application/json")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken()))
+            .andExpect(status().isOk()).andReturn();
 
-        when(scheduleRepository.findById(any(Long.class)))
-                .thenReturn(opt);
+    verify(mockScheduleRepository, times(1)).findById(1L);
 
-        MvcResult response = mockMvc.perform(get(url).contentType("application/json")).andExpect(status().isOk())
-                .andReturn();
+    String responseString = response.getResponse().getContentAsString();
+    String actualString = s1.toString();
+    assertEquals(actualString, responseString);
+  }
 
-        String responseString = response.getResponse().getContentAsString();
+  @Test
+    public void testCreateSchedule() throws Exception {
+      Schedule s1 = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "123456");
+      when(mockScheduleRepository.save(any(Schedule.class))).thenReturn(s1);
+      MvcResult response = mockMvc
+          .perform(post("/api/public/createSchedule?name=CS 156&description=Adv App Programming&quarter=Fall 2020&userId=123456").with(csrf()).contentType(MediaType.APPLICATION_JSON)
+              .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken()))
+          .andExpect(status().isOk()).andReturn();
 
-        assertEquals(expectedResult, responseString);
+
+      String responseString = response.getResponse().getContentAsString();
+      String expectedString = "Schedule[ id=1, name=CS 156, description=Adv App Programming, quarter=Fall 2020, userId=123456 ]";
+      assertEquals(responseString, expectedString);
     }
 
-    @Test
-    public void test_getScheduleFailureNoSchedule() throws Exception {
-        String expectedResult = "";
-        String urlTemplate = "/api/public/getSchedule?id=%s";
-        String url = String.format(urlTemplate, "1");
+  @Test
+    public void testGetScheduleFailure() throws Exception {
+      Optional<Schedule> expectedSchedules = Optional.empty();
 
-        //Schedule schedule = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "s");
+      when(mockScheduleRepository.findById(1L)).thenReturn(expectedSchedules);
+      MvcResult response =
+          mockMvc
+              .perform(get("/api/public/getSchedule?id=1").contentType("application/json")
+                  .header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken()))
+              .andExpect(status().isBadRequest()).andReturn();
 
-        Optional<Schedule> opt = Optional.empty();
+      verify(mockScheduleRepository, times(1)).findById(1L);
 
-        when(scheduleRepository.findById(any(Long.class)))
-                .thenReturn(opt);
-
-        MvcResult response = mockMvc.perform(get(url).contentType("application/json")).andExpect(status().isBadRequest())
-                .andReturn();
-
-        String responseString = response.getResponse().getContentAsString();
-
-        assertEquals(expectedResult, responseString);
+      String responseString = response.getResponse().getContentAsString();
+      String actualString = "";
+      assertEquals(actualString, responseString);
     }
 
-    @Test
-    public void test_createScheduleSuccess() throws Exception {
-        String expectedResult = "Schedule[ id=1, name=CS 156, description=Adv App Programming, quarter=Fall 2020, userId=s ]";
-        String urlTemplate = "/api/public/createSchedule?name=%s&description=%s&quarter=%s&userId=%s";
-        String url = String.format(urlTemplate, "CS 156", "Adv App Programming", "Fall 2020", "s");
+@Test
+    public void testDeleteSchedule_scheduleExists() throws Exception {
+      Schedule s1 = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "123456");
+      when(mockScheduleRepository.findById(1L)).thenReturn(Optional.of(s1));
+      MvcResult response = mockMvc
+          .perform(delete("/api/public/deleteSchedule?id=1").with(csrf()).contentType(MediaType.APPLICATION_JSON)
+              .characterEncoding("utf-8").header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken()))
+          .andExpect(status().isNoContent()).andReturn();
+      verify(mockScheduleRepository, times(1)).findById(s1.getId());
+      verify(mockScheduleRepository, times(1)).deleteById(s1.getId());
 
-        Schedule schedule = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "s");
+      String responseString = response.getResponse().getContentAsString();
 
-        when(scheduleRepository.save(any(Schedule.class)))
-                .thenReturn(schedule);
-
-        MvcResult response = mockMvc.perform(get(url).contentType("application/json")).andExpect(status().isOk())
-                .andReturn();
-
-        String responseString = response.getResponse().getContentAsString();
-
-        assertEquals(expectedResult, responseString);
+      assertEquals(responseString.length(), 0);
     }
+  
+@Test
+  public void testDeleteSchedule_scheduleNotFound() throws Exception {
+    long id = 1L;
+    when(mockScheduleRepository.findById(id)).thenReturn(Optional.empty());
+    mockMvc
+        .perform(delete("/api/public/deleteSchedule?id=1").with(csrf()).contentType(MediaType.APPLICATION_JSON)
+            .characterEncoding("utf-8").header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken()))
+        .andExpect(status().isNotFound()).andReturn();
+    verify(mockScheduleRepository, times(1)).findById(id);
+    verify(mockScheduleRepository, times(0)).deleteById(id);
+  }
 
-    @Test
-    public void test_deleteScheduleSuccess() throws Exception {
-        String expectedResult = "";
-        String urlTemplate = "/api/public/deleteSchedule?id=%s";
-        String url = String.format(urlTemplate, "1");
-
-        Schedule schedule = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "s");
-
-        //when(scheduleRepository.deleteById(any(Long.class))).then(doNothing());\
-        //doNothing().when(scheduleRepository.deleteById(any(Long.class)));
-
-        MvcResult response = mockMvc.perform(get(url).contentType("application/json")).andExpect(status().isOk())
-                .andReturn();
-
-        String responseString = response.getResponse().getContentAsString();
-
-        assertEquals(expectedResult, responseString);
-    }
-
-
-
-
-
+@Test
+  public void testDeleteTodo_todoNotOwned() throws Exception {
+    Schedule s1 = new Schedule(1L,"CS 156", "Adv App Programming", "Fall 2020", "NOT YOURS");
+    when(mockScheduleRepository.findById(s1.getId())).thenReturn(Optional.of(s1));
+    mockMvc
+        .perform(delete("/api/public/deleteSchedule?id=1").with(csrf()).contentType(MediaType.APPLICATION_JSON)
+            .characterEncoding("utf-8").header(HttpHeaders.AUTHORIZATION, "Bearer " + userToken()))
+        .andExpect(status().isNotFound()).andReturn();
+    verify(mockScheduleRepository, times(1)).findById(s1.getId());
+    verify(mockScheduleRepository, times(0)).deleteById(s1.getId());
+  }
 }
