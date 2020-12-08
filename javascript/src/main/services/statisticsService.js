37f32efb12d1be170edf5ffb38d28e3d01bccc59
Paritosh <paritoshjha@cs.ucsb.edu>
2020-12-07 23:12:11 -0800

    add fetch for start and end quarter

--- javascript/src/main/services/statisticsService.js
+++ javascript/src/main/services/statisticsService.js
@@ -6,11 +6,12 @@ const fetchCourseCount = async () => {
     return courseCountResponse.json();
 }
 
-const fetchClassSize = async () => {
-    const url = `/api/public/statistics/classSize`;
+const fetchClassSize = async (fields) => {
+    const url = `/api/public/statistics/classSize?startQuarter=${encodeURIComponent(fields.startQuarter)}&endQuarter=${encodeURIComponent(fields.endQuarter)}`;
     const classSizeResponse = await fetch(url);
     return classSizeResponse.json();
 }
+
 
 export { fetchCourseCount, fetchClassSize };
     
