5b8f4e6e67a933befdc8969f10850609ab3cb9d3
Andy Ho <andyho@ucsb.edu>
2020-12-07 23:54:43 -0800

    ah - Add functionality so that only user specific schedules appear and can be changed

--- src/main/java/edu/ucsb/courses/controllers/ScheduleController.java
+++ src/main/java/edu/ucsb/courses/controllers/ScheduleController.java
@@ -16,9 +16,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
