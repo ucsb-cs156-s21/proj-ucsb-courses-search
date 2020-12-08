ffe5705c53894cdf1af01f6b63c8c4a30dc1691c
Mitch <mitchell5799@gmail.com>
2020-12-08 15:30:10 -0800

    ML - Fixed name of ScheduleItem repository findBySchedule_id

--- src/main/java/edu/ucsb/courses/repositories/ScheduleItemRepository.java
+++ src/main/java/edu/ucsb/courses/repositories/ScheduleItemRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScheduleItemRepository extends CrudRepository<ScheduleItem, Long> {
-  List<ScheduleItem> findByScheduleId(Long scheduleId);
-  void deleteByScheduleId(Long scheduleId);
+  List<ScheduleItem> findBySchedule_id(Long schedule_id);
+  void deleteBySchedule_id(Long schedule_id);
 }
