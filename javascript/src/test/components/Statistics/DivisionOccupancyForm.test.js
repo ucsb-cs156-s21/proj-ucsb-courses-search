55ce90f2117d1c109beb69e242cc3985e3b85e2b
Heather Dinh <heatherdinh@ucsb.edu>
2020-12-08 11:10:11 -0800

    hd - finished frontend tests

---
+++ javascript/src/test/components/Statistics/DivisionOccupancyForm.test.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import fetch from "isomorphic-unfetch";
+jest.mock("isomorphic-unfetch");
+
+import DivisionOccupancyForm from "main/components/Statistics/DivisionOccupancyForm";
+
+describe("DivisionOccupancyForm tests", () => {
+
+    test("renders without crashing", () => {
+        render(<DivisionOccupancyForm />);
+    });
+
+    test("when I select a start quarter, the state for start quarter changes", () => {
+        const { getByTestId } = render(<DivisionOccupancyForm />);
+        const selectQuarter = getByTestId("select-start-quarter")
+        userEvent.selectOptions(selectQuarter, "4");
+        expect(selectQuarter.value).toBe("4");
+    });
+
+    test("when I select an end quarter, the state for end quarter changes", () => {
+        const { getByTestId } = render(<DivisionOccupancyForm />);
+        const selectQuarter = getByTestId("select-end-quarter")
+        userEvent.selectOptions(selectQuarter, "1");
+        expect(selectQuarter.value).toBe("1");
+    });
+
+    test("when I select a department, the state for department changes", () => {
+        const { getByLabelText } = render(<DivisionOccupancyForm />);
+        const selectDepartment = getByLabelText("Department")
+        userEvent.selectOptions(selectDepartment, "MATH ");
+        expect(selectDepartment.value).toBe("MATH ");
+    });
+
+    test("when I select a level, the state for level changes", () => {
+        const { getByLabelText } = render(<DivisionOccupancyForm />);
+        const selectLevel = getByLabelText("Course Level")
+        userEvent.selectOptions(selectLevel, "G");
+        expect(selectLevel.value).toBe("G");
+    });
+
+    test("when I click submit, the right stuff happens", async () => {
+ 
+        const sampleReturnValue = {
+            "sampleKey": "sampleValue"
+        };
+
+        // Create spy functions (aka jest function, magic function)
+        // The function doesn't have any implementation unless
+        // we specify one.  But it does keep track of whether 
+        // it was called, how many times it was called,
+        // and what it was passed.
+
+        const setCourseJSON = jest.fn();
+        const fetchJSONSpy = jest.fn();
+
+        fetchJSONSpy.mockResolvedValue(sampleReturnValue);
+
+        const { getByText, getByLabelText } = render(
+            <DivisionOccupancyForm setCourseJSON={setCourseJSON} fetchJSON={fetchJSONSpy} />
+        );
+
+        const expectedFields = {
+            startQuarter: "20204",
+            endQuarter: "20211",
+            department: "MATH ",
+            level: "G"
+        };
+
+        const selectStartQuarter = getByLabelText("Start Quarter")
+        userEvent.selectOptions(selectStartQuarter, "4");
+        const selectEndQuarter = getByLabelText("End Quarter")
+        userEvent.selectOptions(selectEndQuarter, "1");
+        const selectDepartment = getByLabelText("Department")
+        userEvent.selectOptions(selectDepartment, "MATH ");
+        const selectLevel = getByLabelText("Course Level")
+        userEvent.selectOptions(selectLevel, "G");
+
+        const submitButton = getByText("Submit");
+        userEvent.click(submitButton);
+
+        // we need to be careful not to assert this expectation
+        // until all of the async promises are resolved
+        await waitFor(() => expect(setCourseJSON).toHaveBeenCalledTimes(1));
+        await waitFor(() => expect(fetchJSONSpy).toHaveBeenCalledTimes(1));
+
+        // assert that ourSpy was called with the right value
+        expect(setCourseJSON).toHaveBeenCalledWith(sampleReturnValue);
+        expect(fetchJSONSpy).toHaveBeenCalledWith(expectedFields);
+
+    });
+
+});
+
---
+++ javascript/src/test/services/statisticsService.test.js
@@ -0,0 +1,31 @@
+import { fetchDivisionOccupancy } from "main/services/statisticsService";
+
+import fetch from "isomorphic-unfetch";
+jest.mock("isomorphic-unfetch");
+
+describe("courseCount tests",  () => {
+  test("fetchDivisionOccupancy", async () => {
+
+    const sampleReturnValue = {
+        "sampleKey": "sampleValue"
+    };
+
+    fetch.mockResolvedValue({
+        status: 200,
+        json: () => {
+          return sampleReturnValue;
+        },
+      });
+
+    const expectedFields = {
+        startQuarter: "20201",
+        endQuarter: "20204",
+        department: "MATH ",
+        level: "U"
+    };
+
+    const result = await fetchDivisionOccupancy(expectedFields);
+    expect(result).toBe(sampleReturnValue);
+
+  });
+});
