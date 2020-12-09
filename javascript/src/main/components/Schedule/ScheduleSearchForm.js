7d0b388b830eacbe9938aa003ef90da143851933
Icarus99 <jiajun_li@ucsb.edu>
2020-12-09 17:16:48 +0800

    added correct user token

--- javascript/src/main/components/Schedule/ScheduleSearchForm.js
+++ javascript/src/main/components/Schedule/ScheduleSearchForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import fetch from "isomorphic-unfetch";
+import useSWR from "swr";
+import { useAuth0 } from "@auth0/auth0-react";
+import { fetchWithToken } from "main/utils/fetch";
 
 const ScheduleSearchForm = ({ setScheduleJSON, fetchJSON }) => {
 
@@ -9,13 +12,25 @@ const ScheduleSearchForm = ({ setScheduleJSON, fetchJSON }) => {
     const [name, setName] = useState("Schedule0");
     const [description, setDescription] = useState("Default Description");
     const [quarter, setQuarter] = useState("Fall20");
-    const [authorization, setAuthorization] = useState("admin");
+    const { user, getAccessTokenSilently } = useAuth0();
+    
+    const getAccessToken = async () => {
+      try {
+        const token = await getAccessTokenSilently()
+        console.log(token);
+          return token;
+      } catch (e) {
+        
+      }
+    }
+    
+    const [authorization, setAuthorization] = useState(getAccessToken);
     const [id,setId] = useState("0");
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("submit pressed");
-        fetchJSON(event, {authorization,id}).then((scheduleJSON)=> {
+        fetchJSON(event, {authorization}).then((scheduleJSON)=> {
             setScheduleJSON(scheduleJSON);
         });
     };
