58ba4a4330716d51d907df5f7987a38c96c8b8a3
Andy Ho <andyho@ucsb.edu>
2020-12-08 17:35:22 -0800

    ah - Implemented use of object mapper to populate responses

--- src/main/java/edu/ucsb/courses/controllers/ScheduleController.java
+++ src/main/java/edu/ucsb/courses/controllers/ScheduleController.java
@@ -1,6 +1,7 @@
 package edu.ucsb.courses.controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import javax.validation.Valid;
@@ -10,76 +11,98 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.ucsb.courses.entities.Schedule;
+import edu.ucsb.courses.repositories.ScheduleRepository;
 import edu.ucsb.courses.services.UCSBCurriculumService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-import edu.ucsb.courses.documents.Course;
-import edu.ucsb.courses.documents.CoursePage;
-import edu.ucsb.courses.repositories.ScheduleRepository;
-
 
 @RestController
-@RequestMapping("/api/public")
 public class ScheduleController {
-    private final Logger logger = LoggerFactory.getLogger(BasicSearchController.class);
+  private final Logger logger = LoggerFactory.getLogger(ScheduleController.class);
 
-    private ObjectMapper mapper = new ObjectMapper();
+  @Autowired
+  private ScheduleRepository scheduleRepository;
 
-    @Autowired
-    ScheduleRepository scheduleRepository;
+  private ObjectMapper mapper = new ObjectMapper();
 
-    @PostMapping(value = "/createSchedule", produces = "application/json")
-    public ResponseEntity<String> createSchedule(@RequestParam String name,
-                                                 @RequestParam String description,
-                                                 @RequestParam String quarter,
-                                                 @RequestHeader("Authorization") String authorization){
-        DecodedJWT jwt = JWT.decode(authorization.substring(7));
-        Schedule newSched = new Schedule(null, name, description, quarter, jwt.getSubject());
-        Schedule savedSched= scheduleRepository.save(newSched);
-        return ResponseEntity.ok().body(savedSched.toString());
+  @GetMapping(value = "/api/schedules", produces = "application/json")
+  public ResponseEntity<String> getUserSchedules(@RequestHeader("Authorization") String authorization)
+      throws JsonProcessingException {
+    DecodedJWT jwt = JWT.decode(authorization.substring(7));
+    List<Schedule> schedules = scheduleRepository.findByUserId(jwt.getSubject());
+    ObjectMapper mapper = new ObjectMapper();
+
+    String body = mapper.writeValueAsString(schedules);
+    return ResponseEntity.ok().body(body);
+  }
+
+  @GetMapping(value = "/api/schedules/{id}", produces = "application/json")
+  public ResponseEntity<String> getSchedule(@RequestHeader("Authorization") String authorization, 
+                                             @PathVariable("id") Long id) 
+      throws JsonProcessingException {
+    DecodedJWT jwt = JWT.decode(authorization.substring(7));
+    Optional<Schedule> schedule = scheduleRepository.findById(id);
+    if (schedule.isEmpty() || !schedule.get().getUserId().equals(jwt.getSubject())) {
+      return ResponseEntity.notFound().build();
     }
 
-    @DeleteMapping(value = "/deleteSchedule", produces = "application/json")
-    public ResponseEntity<String> deleteSchedule(@RequestHeader("Authorization") String authorization, @RequestParam String id){
-        DecodedJWT jwt = JWT.decode(authorization.substring(7));
-        Long castId = Long.parseLong(id);
-        Optional<Schedule> target = scheduleRepository.findById(castId);
-        if (!target.isPresent() || !target.get().getUserId().equals(jwt.getSubject())) {
-            return ResponseEntity.notFound().build();
-        }
-        scheduleRepository.deleteById(castId);
-        return ResponseEntity.noContent().build();
+    ObjectMapper mapper = new ObjectMapper();
+    String body = mapper.writeValueAsString(schedule.get());
+    return ResponseEntity.ok().body(body);
+  }
+
+  @PostMapping(value = "/api/schedules", produces = "application/json")
+  public ResponseEntity<String> createSchedule(@RequestHeader("Authorization") String authorization,
+      @RequestBody @Valid Schedule schedule) throws JsonProcessingException {
+    DecodedJWT jwt = JWT.decode(authorization.substring(7));
+    schedule.setUserId(jwt.getSubject());
+    Schedule savedSchedule = scheduleRepository.save(schedule);
+    String body = mapper.writeValueAsString(savedSchedule);
+    return ResponseEntity.ok().body(body);
+  }
+
+  @DeleteMapping(value = "/api/schedules/{id}", produces = "application/json")
+  public ResponseEntity<String> deleteTodo(@RequestHeader("Authorization") String authorization,
+      @PathVariable("id") Long id) {
+    DecodedJWT jwt = JWT.decode(authorization.substring(7));
+    Optional<Schedule> schedule = scheduleRepository.findById(id);
+    if (!schedule.isPresent() || !schedule.get().getUserId().equals(jwt.getSubject())) {
+      return ResponseEntity.notFound().build();
+    }
+    scheduleRepository.deleteById(id);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PutMapping(value = "/api/schedules/{id}", produces = "application/json")
+  public ResponseEntity<String> updateTodo(@RequestHeader("Authorization") String authorization,
+      @PathVariable("id") Long id, @RequestBody @Valid Schedule incomingSchedule)
+      throws JsonProcessingException {
+    DecodedJWT jwt = JWT.decode(authorization.substring(7));
+    Optional<Schedule> schedule = scheduleRepository.findById(id);
+    if (!schedule.isPresent() || !schedule.get().getUserId().equals(jwt.getSubject())) {
+      return ResponseEntity.notFound().build();
     }
 
-    @GetMapping(value = "/getSchedule", produces = "application/json")
-    public ResponseEntity<String> getSchedule(@RequestHeader("Authorization") String authorization, @RequestParam String id) 
-        throws JsonProcessingException{
-        DecodedJWT jwt = JWT.decode(authorization.substring(7));
-        Long castId = Long.parseLong(id);
-        Optional<Schedule> target = scheduleRepository.findById(castId);
-        if (target.isPresent()) {
-            if (target.get().getUserId().equals(jwt.getSubject())){
-                String body = target.get().toString();
-                return ResponseEntity.ok().body(body);
-            }
-        }
-        return ResponseEntity.badRequest().build();
+    if (!incomingSchedule.getId().equals(id) || !incomingSchedule.getUserId().equals(jwt.getSubject())) {
+      return ResponseEntity.badRequest().build();
     }
 
-
+    scheduleRepository.save(incomingSchedule);
+    String body = mapper.writeValueAsString(incomingSchedule);
+    return ResponseEntity.ok().body(body);
+  }
 }
