5e31d59ef5f9e40c6452028198e296a265532d34
zhuang99 <zilie_huang@umail.ucsb.edu>
2020-12-09 13:00:00 +0800

    ZH - schedule add form implemented

--- javascript/src/main/pages/Schedule/Schedule.js
+++ javascript/src/main/pages/Schedule/Schedule.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron,Form } from "react-bootstrap";
 import ScheduleSearchForm from "main/components/Schedule/ScheduleSearchForm";
 import ScheduleTable from "main/components/Schedule/ScheduleTable";
 import ScheduleCoursesTable from "main/components/Schedule/ScheduleCoursesTable";
+import AddSchedForm from "main/components/Schedule/AddSchedForm";
 
 var data = new Array();
+
 const Schedule = () => {
   return (
         <Jumbotron>
-          <h1>Personal Schedule</h1>
-        <ScheduleSearchForm/>
-          <ScheduleTable courses={data} admin={true}/>
+          <h1>Create Personal Schedule</h1>
+          <AddSchedForm/>
+          <Form.Group> </Form.Group>
 
+         <h1>Load Schedule</h1>
+          <ScheduleSearchForm/>
+          <Form.Group> </Form.Group>
+          <h1>Personal Schedule</h1>
+          <ScheduleTable courses={data} admin={true}/>
+          <Form.Group> </Form.Group>
           <h1>Personal Schedule Courses List</h1>
           <ScheduleCoursesTable classes={data} /> 
           {/* delete courses in this table */}
