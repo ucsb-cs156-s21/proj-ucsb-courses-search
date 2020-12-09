36d19f95c89d87d5b5a3a88ad826e85d1bd4e888
Andy Ho <andyho@ucsb.edu>
2020-12-08 19:27:45 -0800

    ah - Moving back to previous version with small revisions to output in response

--- src/main/java/edu/ucsb/courses/controllers/ScheduleController.java
+++ src/main/java/edu/ucsb/courses/controllers/ScheduleController.java
@@ -11,98 +11,84 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.ucsb.courses.entities.Schedule;
-import edu.ucsb.courses.repositories.ScheduleRepository;
 import edu.ucsb.courses.services.UCSBCurriculumService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import edu.ucsb.courses.documents.Course;
+import edu.ucsb.courses.documents.CoursePage;
+import edu.ucsb.courses.repositories.ScheduleRepository;
+
+
 @RestController
+@RequestMapping("/api/public")
 public class ScheduleController {
-  private final Logger logger = LoggerFactory.getLogger(ScheduleController.class);
+    private final Logger logger = LoggerFactory.getLogger(BasicSearchController.class);
 
-  @Autowired
-  private ScheduleRepository scheduleRepository;
+    private ObjectMapper mapper = new ObjectMapper();
 
-  private ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    ScheduleRepository scheduleRepository;
 
-  @GetMapping(value = "/api/schedules", produces = "application/json")
-  public ResponseEntity<String> getUserSchedules(@RequestHeader("Authorization") String authorization)
-      throws JsonProcessingException {
-    DecodedJWT jwt = JWT.decode(authorization.substring(7));
-    List<Schedule> schedules = scheduleRepository.findByUserId(jwt.getSubject());
-    ObjectMapper mapper = new ObjectMapper();
-
-    String body = mapper.writeValueAsString(schedules);
-    return ResponseEntity.ok().body(body);
-  }
-
-  @GetMapping(value = "/api/schedules/{id}", produces = "application/json")
-  public ResponseEntity<String> getSchedule(@RequestHeader("Authorization") String authorization, 
-                                             @PathVariable("id") Long id) 
-      throws JsonProcessingException {
-    DecodedJWT jwt = JWT.decode(authorization.substring(7));
-    Optional<Schedule> schedule = scheduleRepository.findById(id);
-    if (schedule.isEmpty() || !schedule.get().getUserId().equals(jwt.getSubject())) {
-      return ResponseEntity.notFound().build();
+    @PostMapping(value = "/createSchedule", produces = "application/json")
+    public ResponseEntity<String> createSchedule(@RequestParam String name,
+                                                 @RequestParam String description,
+                                                 @RequestParam String quarter,
+                                                 @RequestHeader("Authorization") String authorization) throws JsonProcessingException {
+        DecodedJWT jwt = JWT.decode(authorization.substring(7));
+        Schedule newSched = new Schedule(null, name, description, quarter, jwt.getSubject());
+        Schedule savedSched= scheduleRepository.save(newSched);
+        return ResponseEntity.ok().body(mapper.writeValueAsString(savedSched));
     }
 
-    ObjectMapper mapper = new ObjectMapper();
-    String body = mapper.writeValueAsString(schedule.get());
-    return ResponseEntity.ok().body(body);
-  }
-
-  @PostMapping(value = "/api/schedules", produces = "application/json")
-  public ResponseEntity<String> createSchedule(@RequestHeader("Authorization") String authorization,
-      @RequestBody @Valid Schedule schedule) throws JsonProcessingException {
-    DecodedJWT jwt = JWT.decode(authorization.substring(7));
-    schedule.setUserId(jwt.getSubject());
-    Schedule savedSchedule = scheduleRepository.save(schedule);
-    String body = mapper.writeValueAsString(savedSchedule);
-    return ResponseEntity.ok().body(body);
-  }
-
-  @DeleteMapping(value = "/api/schedules/{id}", produces = "application/json")
-  public ResponseEntity<String> deleteTodo(@RequestHeader("Authorization") String authorization,
-      @PathVariable("id") Long id) {
-    DecodedJWT jwt = JWT.decode(authorization.substring(7));
-    Optional<Schedule> schedule = scheduleRepository.findById(id);
-    if (!schedule.isPresent() || !schedule.get().getUserId().equals(jwt.getSubject())) {
-      return ResponseEntity.notFound().build();
-    }
-    scheduleRepository.deleteById(id);
-    return ResponseEntity.noContent().build();
-  }
-
-  @PutMapping(value = "/api/schedules/{id}", produces = "application/json")
-  public ResponseEntity<String> updateTodo(@RequestHeader("Authorization") String authorization,
-      @PathVariable("id") Long id, @RequestBody @Valid Schedule incomingSchedule)
-      throws JsonProcessingException {
-    DecodedJWT jwt = JWT.decode(authorization.substring(7));
-    Optional<Schedule> schedule = scheduleRepository.findById(id);
-    if (!schedule.isPresent() || !schedule.get().getUserId().equals(jwt.getSubject())) {
-      return ResponseEntity.notFound().build();
+    @DeleteMapping(value = "/deleteSchedule", produces = "application/json")
+    public ResponseEntity<String> deleteSchedule(@RequestHeader("Authorization") String authorization, @RequestParam String id){
+        DecodedJWT jwt = JWT.decode(authorization.substring(7));
+        Long castId = Long.parseLong(id);
+        Optional<Schedule> target = scheduleRepository.findById(castId);
+        if (!target.isPresent() || !target.get().getUserId().equals(jwt.getSubject())) {
+            return ResponseEntity.notFound().build();
+        }
+        scheduleRepository.deleteById(castId);
+        return ResponseEntity.noContent().build();
     }
 
-    if (!incomingSchedule.getId().equals(id) || !incomingSchedule.getUserId().equals(jwt.getSubject())) {
-      return ResponseEntity.badRequest().build();
+    @GetMapping(value = "/getSchedule", produces = "application/json")
+    public ResponseEntity<String> getSchedule(@RequestHeader("Authorization") String authorization, @RequestParam String id) 
+        throws JsonProcessingException{
+        DecodedJWT jwt = JWT.decode(authorization.substring(7));
+        Long castId = Long.parseLong(id);
+        Optional<Schedule> target = scheduleRepository.findById(castId);
+        if (target.isPresent() && target.get().getUserId().equals(jwt.getSubject())) {
+          String body = target.get().toString();
+          return ResponseEntity.ok().body(mapper.writeValueAsString(target.get()));
+        }
+        return ResponseEntity.badRequest().build();
     }
 
-    scheduleRepository.save(incomingSchedule);
-    String body = mapper.writeValueAsString(incomingSchedule);
-    return ResponseEntity.ok().body(body);
-  }
+    @GetMapping(value = "/getSchedules", produces = "application/json")
+      public ResponseEntity<String> getSchedules(@RequestHeader("Authorization") String authorization, @RequestParam String id) 
+          throws JsonProcessingException{
+          DecodedJWT jwt = JWT.decode(authorization.substring(7));
+          List<Schedule> savedSchedules= scheduleRepository.findByUserId(jwt.getSubject());
+          String res = "";
+          for (Schedule sched: savedSchedules){
+            res = res.concat(mapper.writeValueAsString(sched) + "!");
+          }
+          if (res.length() == 0) {
+            return ResponseEntity.noContent().build();
+          }
+          return ResponseEntity.ok().body(res);
+     }
 }
