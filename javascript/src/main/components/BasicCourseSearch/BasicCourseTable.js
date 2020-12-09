bee5f3077a273a0d7fdd327906a68640ad098a3f
Andrew Berry <andrewberry@umail.ucsb.edu>
2020-12-09 14:52:51 -0800

    AB - Removed unnecessary console logs, made render outputs not in spans, removed unnecessary comments

--- javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
+++ javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
@@ -3,21 +3,6 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import { Button } from "react-bootstrap";
 const BasicCourseTable = ( {classes} ) => {
   const sections = [];
-/*  classes.forEach(
-    (course) => {
-      course.classSections.forEach(
-        (section) => {
-          section.course =
-          {
-            courseId: course.courseId,
-            title: course.title,
-            unitsFixed: course.unitsFixed
-          };
-          sections.push(section);
-        }
-      )
-    }
-  );*/
 
   var numSections = 0;
   classes.slice().reverse().forEach(
@@ -55,30 +40,25 @@ const BasicCourseTable = ( {classes} ) => {
   sections.reverse();
   
 const rowStyle = (row, rowIndex) => {
-  // console.log(`row=`,row, `rowIndex`, rowIndex);
   return (row.section % 100 == 0)? {backgroundColor: '#CEDEFA'}: (rowIndex % 2 == 1)? {backgroundColor: '#EDF3FE'}: {backgroundColor: '#FFFFFF'}
 }
 const dataAlignment = (cell, row) => {
-  // console.log(`cell=${cell} row=`, row);
   const alignmnet = (row.section % 100 == 0)? 'left': 'right';
   return alignmnet
 }
 const renderCourseId = (cell, row) => {
-  // console.log(`cell=${cell} row=`, row);
   const courseId = (row.section % 100 == 0)? row.course.courseId: "";
-  return (  <span>{courseId}</span> )
+  return ( courseId )
 }
 const renderCourseTitle = (cell, row) => {
   const courseTitle = (row.section % 100 == 0)? row.course.title: "";
-  return (  <span>{courseTitle}</span> )
+  return ( courseTitle )
 }
 const renderInstructors = (cell, row) => {
-  // console.log(`cell=${cell} row=`, row);
   const instructor = (row.instructors.length > 0)? row.instructors[0].instructor: "TBD";
-  return (  <span>{instructor}</span> )
+  return ( instructor )
 }
 const renderAddCourseButton = (id,cell,row) => {
-  console.log(`cell=${cell} row=`, row);
   const button = (row.section % 100 != 0 || row.course.noSections == "true")? 
     <Button data-testid="add-course-button" onClick={() => { console.log("To Be Implemented")}}>Add Course</Button>: "";
   return (
@@ -115,14 +95,14 @@ const renderAddCourseButton = (id,cell,row) => {
     align: (cell, row) => dataAlignment(cell, row)
   }
 ];
-//if (member) {
+
   columns.push({
       text: "",
       isDummyField: true,
       dataField: "add-course",
       formatter: (cell, row) => renderAddCourseButton(row.id, cell, row)
   });
-//}
+
   return (
     <BootstrapTable keyField='enrollCode' data={sections} columns={columns} rowStyle={rowStyle}/>
   );
