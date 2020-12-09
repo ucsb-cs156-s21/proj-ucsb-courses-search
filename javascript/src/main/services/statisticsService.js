474aaac0a825823a0679ef82582b82a3f5505969
Heather Dinh <heatherdinh@ucsb.edu>
2020-12-08 19:11:19 -0800

    hd - fix for frontend coverage

--- javascript/src/main/services/statisticsService.js
+++ javascript/src/main/services/statisticsService.js
@@ -4,7 +4,7 @@ const fetchDivisionOccupancy = async (fields) => {
     const url = `/api/public/statistics/courseOccupancyByDivision?startQuarter=${fields.startQuarter}&endQuarter=${fields.endQuarter}&department=${fields.department}&level=${fields.level}`;
     const divisionOccupancyResponse = await fetch(url);   
     return divisionOccupancyResponse.json();
-}
+} 
 
 
 export { fetchDivisionOccupancy };
