ae2679ee6a23a59effcaac30bad659806c0d4b4d
Icarus99 <jiajun_li@ucsb.edu>
2020-12-09 13:52:00 +0800

    added schedule service

--- javascript/src/main/pages/Schedule/Schedule.js
+++ javascript/src/main/pages/Schedule/Schedule.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import { Jumbotron,Form } from "react-bootstrap";
 import ScheduleSearchForm from "main/components/Schedule/ScheduleSearchForm";
 import ScheduleTable from "main/components/Schedule/ScheduleTable";
 import ScheduleCoursesTable from "main/components/Schedule/ScheduleCoursesTable";
+import JSONPrettyCard from "main/components/Utilities/JSONPrettyCard";
 import AddSchedForm from "main/components/Schedule/AddSchedForm";
+import { fetchgetSchedulesJSON } from "main/services/scheduleAPI";
 
 var data = new Array();
 
 const Schedule = () => {
+     const initialScheduleJSON = {
+        "pageNumber": 1,
+        "pageSize": 1,
+        "total": 0,
+        "schedules": []
+    };
+    const [scheduleJSON, setScheduleJSON] = useState(initialScheduleJSON);
   return (
         <Jumbotron>
           <h1>Create Personal Schedule</h1>
@@ -15,10 +25,14 @@ const Schedule = () => {
           <Form.Group> </Form.Group>
 
          <h1>Load Schedule</h1>
-          <ScheduleSearchForm/>
+          <ScheduleSearchForm setScheduleJSON={setScheduleJSON} fetchJSON={fetchgetSchedulesJSON }/>
           <Form.Group> </Form.Group>
           <h1>Personal Schedule</h1>
           <ScheduleTable courses={data} admin={true}/>
+            <JSONPrettyCard
+                    expression={"scheduleJSON"}
+                    value={scheduleJSON}
+                />
           <Form.Group> </Form.Group>
           <h1>Personal Schedule Courses List</h1>
           <ScheduleCoursesTable classes={data} /> 
