1546e5b9e7e9251b2035dabf5a6eee6e64875244
Mitch <mitchell5799@gmail.com>
2020-12-10 20:38:46 -0800

    ML - Added testing suite for AddSchedForm.js

--- javascript/src/test/pages/Statistics/Statistics.test 2.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Statistics from "main/pages/Statistics/Statistics";
-describe("Statistics page tests", () => {
-  test("renders without crashing", () => {
-    render(<Statistics />);
-  });
-});
