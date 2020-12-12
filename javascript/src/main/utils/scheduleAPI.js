8f0404ac80003e1fcaf508566ac5dcd8dd5310d4
Mitch <mitchell5799@gmail.com>
2020-12-11 19:02:04 -0800

    ML - whoops scheduleAPI wasn't staged

---
+++ javascript/src/main/utils/scheduleAPI.js
@@ -0,0 +1,81 @@
+import { fetchWithToken } from "main/utils/fetch"
+
+
+
+//schedule
+const fetchcreateScheduleJSON = async (schedule, getToken, onSuccess, onError) => {
+    const token = await getToken();
+    const url = "/api/public/createSchedule?" + new URLSearchParams(schedule);
+    try {
+        const json = await fetchWithToken(url, getToken, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "content-type": "application/json"
+            }
+        });
+        onSuccess(json);
+    } catch (e) {
+        onError(e);
+    }
+}
+
+const fetchdeleteScheduleJSON = async (id, getToken, onSuccess, onError) => {
+    const token = await getToken();
+    const url = "/api/public/deleteSchedule?" + new URLSearchParams(id);
+    
+    try {
+        const deleteSchedule = await fetchWithToken(url, getToken, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "content-type": "application/json"
+            },
+            noJSON: true,
+        });
+        onSuccess();
+    } catch (e) {
+        onError(e);
+    }
+}
+
+const fetchgetScheduleJSON = async (id, getToken, onSuccess, onError) => {
+    const token = await getToken();
+    const url = "/api/public/getSchedule?" + new URLSearchParams(id);
+    console.log("Getting schedule...");
+    try {
+        const json = await fetchWithToken(url, getToken, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "content-type": "application/json"
+            }
+        });
+        onSuccess(json);
+        return json;
+    } catch (e) {
+        onError(e);
+    }
+}
+
+const fetchgetSchedulesJSON = async (getToken, onSuccess, onError) => {
+    const token = await getToken();
+    const url = "/api/public/getSchedules";
+    
+    try {
+        const json = await fetchWithToken(url, getToken, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "content-type": "application/json"
+
+            }
+        });
+        onSuccess(json);
+    } catch (e) {
+        onError(e);
+    }
+}
+
+export { fetchcreateScheduleJSON, fetchdeleteScheduleJSON, fetchgetScheduleJSON, fetchgetSchedulesJSON};
+    
