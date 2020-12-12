78a51d02be44e25b8fd9cc2d69bc8605b5f5b2c2
seanaocostello <sean.ao.costello@gmail.com>
2020-12-12 12:24:17 -0800

    Added tests to Schedule.test.js to test that getSchedule, deleteSchedule and createSchedule buttons actually run the code they are supposed to

--- javascript/src/test/pages/Schedule/Schedule.test.js
+++ javascript/src/test/pages/Schedule/Schedule.test.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Schedule from "main/pages/Schedule/Schedule";
-import {fetchCreateScheduleJSON, fetchGetScheduleJSON} from "main/services/scheduleAPI";
+import {fetchCreateScheduleJSON, fetchGetScheduleJSON, fetchDeleteScheduleJSON} from "main/services/scheduleAPI";
 import { useAuth0 } from "@auth0/auth0-react";
 import {fetchWithToken} from "main/utils/fetch";
+import userEvent from "@testing-library/user-event";
 jest.mock("@auth0/auth0-react");
 import useSWR from "swr";
 jest.mock("swr");
 import { waitFor } from "@testing-library/react";
 jest.mock("main/utils/fetch");
+jest.mock("main/services/scheduleAPI")
 
 
 describe("Schedule tests", () => {
@@ -37,5 +39,59 @@ describe("Schedule tests", () => {
     expect(getByText("Schedule Name")).toBeInTheDocument();
     expect(getByText("Get Schedule")).toBeInTheDocument();
   });
+
+  test("create schedule clicks correctly", async () => {
+      const { getByText } =render(<Schedule />);
+
+      const sampleReturnValue = {
+        "id":1,
+        "name": "first",
+        "description": "first schedule",
+        "quarter": "fall 2020",
+        "userId": "123456"
+      };
+
+      fetchCreateScheduleJSON.mockResolvedValue(sampleReturnValue);
+
+      const submitButton = getByText("Create Schedule");
+      userEvent.click(submitButton);
+      await waitFor(() => expect(fetchCreateScheduleJSON).toHaveBeenCalledTimes(1));
+
+    });
+
+    test("Get Schedule clicks correctly", async () => {
+        const { getByText } =render(<Schedule />);
+
+        const sampleReturnValue = {
+          "id":1,
+          "name": "first",
+          "description": "first schedule",
+          "quarter": "fall 2020",
+          "userId": "123456"
+        };
+
+        fetchGetScheduleJSON.mockResolvedValue(sampleReturnValue);
+        const submitButton = getByText("Get Schedule");
+        userEvent.click(submitButton);
+        await waitFor(() => expect(fetchGetScheduleJSON).toHaveBeenCalledTimes(1));
+    });
+
+    test("Delete Schedule clicks correctly", async () => {
+      const { getByText } =render(<Schedule />);
+
+      const sampleReturnValue = {
+        "id":1,
+        "name": "first",
+        "description": "first schedule",
+        "quarter": "fall 2020",
+        "userId": "123456"
+      };
+
+      const submitButton = getByText("Delete Schedule");
+      userEvent.click(submitButton);
+      await waitFor(() => expect(fetchDeleteScheduleJSON).toHaveBeenCalledTimes(1));
+    });
+
+
 
 });
