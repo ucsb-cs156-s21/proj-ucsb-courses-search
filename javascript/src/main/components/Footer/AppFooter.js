9df9a78a9c6f702e51af13109e57013148a47707
Jeffrey  Liu <jeffrey@Jeffreys-MacBook-Pro-2.local>
2021-02-16 17:43:32 -0800

    update footer content

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -3,9 +3,10 @@ import React from "react";
 const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
-      This is a demo of a minimal React frontend + Spring Boot backend app.
-      Check out the source code on{" "}
-      <a href="https://github.com/ucsb-cs156-f20/demo-spring-nextjs-app">
+      This app is a class project of{" "}
+      <a href="https://ucsb-cs156.github.io/">CMPSC 156</a> at <a href="https://www.ucsb.edu/">UCSB</a>.
+      {" "}Check out the source code on{" "}
+      <a href="https://github.com/ucsb-cs156-w21/proj-ucsb-courses-search">
         GitHub
       </a>
       !
