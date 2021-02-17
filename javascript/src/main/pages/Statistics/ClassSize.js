023a4f705e84f5ca4042cf89b7b28aa9512754b7
Zixuan Mao <zixuanmao@ucsb.edu>
2021-02-16 17:32:56 -0800

    zm/hc - add the header of Average Class Size By Department on the page of Average Class Size By Department

--- javascript/src/main/pages/Statistics/ClassSize.js
+++ javascript/src/main/pages/Statistics/ClassSize.js
@@ -20,6 +20,13 @@ const ClassSize = () => {
 
     return (
         <Jumbotron>
+            <Container className="text-left">
+                <h1>Average Class Size By Department</h1>
+                <CourseOccupancyForm setOccupancyJson={setJsonTableData} fetchJSON={fetchCourseOccupancy} onSubmit={() => { setTableVisibility(false) }}/>
+            </Container>
+            <Container style={{ marginTop: "20px" }} className={"text-center"}>
+                {tableVisibility && (tableData.length ? <CourseOccupancyTable data={tableData} /> : "There are no results!")}
+            </Container>
             <ClassSizeForm setClassSizeJSON={setJsonTableData} fetchClassSize={fetchClassSize}/>
             {tableVisibility && (data.length ? <ClassSizeTable data={data} /> : "There are no results!")}
         </Jumbotron>       
