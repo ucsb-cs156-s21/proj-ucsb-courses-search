e3637217fe1ec12f0e1f9aacbfc28bb5a784f028
Steven Huynh-Tran <htransteven@gmail.com>
2021-02-16 17:58:37 -0800

    add target='_blank' to links in footer

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -4,9 +4,18 @@ const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
       This app is a class project of{" "}
-      <a href="https://ucsb-cs156.github.io">CMPSC 156</a> at{" "}
-      <a href="https://ucsb.edu">UCSB</a>. Check out the source code on{" "}
-      <a href="https://github.com/ucsb-cs156-w21/proj-ucsb-courses-search">
+      <a href="https://ucsb-cs156.github.io" target="_blank">
+        CMPSC 156
+      </a>{" "}
+      at{" "}
+      <a href="https://ucsb.edu" target="_blank">
+        UCSB
+      </a>
+      . Check out the source code on{" "}
+      <a
+        href="https://github.com/ucsb-cs156-w21/proj-ucsb-courses-search"
+        target="_blank"
+      >
         GitHub
       </a>
       !
