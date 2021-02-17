83aeeb55e6cdecb19de5e794a3c5dcefec6a1317
Steven Huynh-Tran <htransteven@gmail.com>
2021-02-16 17:55:07 -0800

    add test cases for footer, removed www. from links

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -4,8 +4,8 @@ const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
       This app is a class project of{" "}
-      <a href="https://ucsb-cs156.github.io/">CMPSC 156</a> at <a href="https://www.ucsb.edu/">UCSB</a>.
-      {" "}Check out the source code on{" "}
+      <a href="https://ucsb-cs156.github.io">CMPSC 156</a> at{" "}
+      <a href="https://ucsb.edu">UCSB</a>. Check out the source code on{" "}
       <a href="https://github.com/ucsb-cs156-w21/proj-ucsb-courses-search">
         GitHub
       </a>
