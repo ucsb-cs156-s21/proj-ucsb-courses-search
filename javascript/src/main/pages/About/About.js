775d2f720e569678495654bd7f63e34d3c6e2755
Cole McKim <cpmckim@umail.ucsb.edu>
2021-02-16 17:54:52 -0800

    Added description of page to About page of app

--- javascript/src/main/pages/About/About.js
+++ javascript/src/main/pages/About/About.js
@@ -9,7 +9,10 @@ const About = () => {
           This web application is a class project of
           CMPSC 156 
           at 
-          UC Santa Barbara. 
+          UC Santa Barbara. The main function of this app is to allow users to search specific classes 
+          arranged based on the following inputs: quarter, department and course level. Users will find
+          a list of classes suited to their desires in order of course number from least to greatest. For each course, users will see 
+          the course name, professor name, enroll code, times and unit number along with a list of sections.
           This app is primarily built using the following:
         </p>
         <ul>
