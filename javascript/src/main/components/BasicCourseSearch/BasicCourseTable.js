c26d29540286512bdcf00a5f3e2a270771017cf7
andrewhlu <alu@ucsb.edu>
2021-02-22 15:44:44 -0800

    AL - Remove commented code

--- javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
+++ javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
@@ -3,16 +3,6 @@ import { Button } from "react-bootstrap";
 import BootstrapTable from 'react-bootstrap-table-next';
 import { reformatJSON } from 'main/utils/BasicCourseTableHelpers';
 
-
-
-//const buttonFormatter = (cell, row, rowIndex, formatExtraData) => {
-//  return(
-//    <Button onClick={()=> {}}> add </Button>  //add onClick funtion to add course to actual schedule
-//  );
-//};
-
-
-
 const BasicCourseTable = ( {classes} ) => {
   const columns = [{
         dataField: 'course.courseId',
