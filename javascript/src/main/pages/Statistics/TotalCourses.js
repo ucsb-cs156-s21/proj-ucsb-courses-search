116185ebcaa448ec7d913dfb60cf7dc7a20f190b
Wade Varesio <wadevaresio@gmail.com>
2021-02-23 17:49:53 -0800

    wv/jm - Added dropdown item to Total Courses By Department in the AppNavBar which currently displays the UI from the statistic/classSize page

---
+++ javascript/src/main/pages/Statistics/TotalCourses.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import { Jumbotron } from "react-bootstrap";
+import { fetchClassSize } from "main/services/statisticsService";
+import ClassSizeTable from "main/components/Statistics/ClassSizeTable";
+import ClassSizeForm from "main/components/Statistics/ClassSizeForm";
+
+
+const TotalCourses = () => {
+    const initialData = [];
+    const [data, setData] = useState(initialData);
+    const [tableVisibility, setTableVisibility] = useState(false);
+
+    const setJsonTableData = (json) => {
+        setData(json);
+        setTableVisibility(true);
+    }
+
+
+
+    return (
+        <Jumbotron>
+            <ClassSizeForm setClassSizeJSON={setJsonTableData} fetchClassSize={fetchClassSize}/>
+            {tableVisibility && (data.length ? <ClassSizeTable data={data} /> : "There are no results!")}
+        </Jumbotron>
+
+    );
+};
+
+export default TotalCourses;
