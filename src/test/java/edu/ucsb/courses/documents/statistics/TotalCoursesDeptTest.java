5e01b4c42da7a7131532251fe98b5c98bd6d2aee
Wade Varesio <wadevaresio@gmail.com>
2021-02-24 14:58:59 -0800

    wv - Added TotalCoursesDeptTest.java to test all functionality of the TotalCoursesDept class.

---
+++ src/test/java/edu/ucsb/courses/documents/statistics/TotalCoursesDeptTest.java
@@ -0,0 +1,82 @@
+package edu.ucsb.courses.documents.statistics;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import net.codebox.javabeantester.JavaBeanTester;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TotalCoursesDeptTest {
+    private static Logger logger = LoggerFactory.getLogger(TotalCoursesDept.class);
+
+    @Test
+    public void testGettersAndSetters() throws Exception {
+        // See: https://github.com/codebox/javabean-tester
+        // The classes that should NOT be tested with the Bean are the
+        // ones that set and get List<> instances
+        JavaBeanTester.test(TotalCoursesDept.class);
+    }
+
+    @Test
+    public void test_notEqualNull() throws Exception {
+        TotalCoursesDept td = new TotalCoursesDept();
+        assertNotEquals(td, null);
+    }
+
+    @Test
+    public void test_notEqualAnotherClass() throws Exception {
+        TotalCoursesDept td = new TotalCoursesDept();
+        assertNotEquals(td, new Object());
+    }
+
+    @Test
+    public void test_equalsSelf() throws Exception {
+        TotalCoursesDept td = new TotalCoursesDept("CMPSC", 20);
+        assertEquals(td, td);
+    }
+
+    @Test
+    public void test_equalsAnother() throws Exception {
+        TotalCoursesDept td1 = new TotalCoursesDept("CMPSC",20);
+        TotalCoursesDept td2 = new TotalCoursesDept("CMPSC", 20);
+        assertEquals(td1, td2);
+    }
+
+    @Test
+    public void test_hashCode() throws Exception {
+        TotalCoursesDept td1 = new TotalCoursesDept("CMPSC", 20);
+        TotalCoursesDept td2 = new TotalCoursesDept("CMPSC",20);
+        assertEquals(td1.hashCode(), td2.hashCode());
+    }
+
+    @Test
+    public void test_toString() throws Exception {
+        TotalCoursesDept td = new TotalCoursesDept("CMPSC", 20);
+        String expected="{ _id='CMPSC', totalCourses='20'}";
+        assertEquals(expected, td.toString());
+    }
+
+    @Test
+    public void test_fromJSON_withError() throws Exception {
+        String badJSON = "this is not good json!";
+        List<TotalCoursesDept> ltd = TotalCoursesDept.listFromJSON(badJSON);
+        assertNull(ltd);
+    }
+
+    @Test
+    public void test_fromJSON() throws Exception {
+        String json = "[ { \"_id\": \"CMPSC\",\n" +
+                "\"totalCourses\": 20}]";
+
+        List<TotalCoursesDept> ltd = TotalCoursesDept.listFromJSON(json);
+
+        TotalCoursesDept expectedTotalCourse = new TotalCoursesDept("CMPSC", 20);
+        assertEquals(expectedTotalCourse, ltd.get(0));
+    }
+}
