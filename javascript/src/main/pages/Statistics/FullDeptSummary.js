f0fe80607041b38690d7ee45bae54d4a9c690c48
Matt Reddick <matthewreddick@cs.ucsb.edu>
2021-02-24 13:33:42 -0800

    mr - new file for Full Dept Summary

---
+++ javascript/src/main/pages/Statistics/FullDeptSummary.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+import { fetchFullCourses } from "main/services/statisticsService";
+import FullDeptForm from "main/components/Statistics/FullDeptForm";
+import FullDeptTable from "main/components/Statistics/FullDeptTable";
+import { fromFormat } from "main/components/Statistics/QuarterFormSelect";
+
+const FullDeptSummary = () => {
+    const [tableVisibility, setTableVisibility] = useState(false);
+    const [tableData, setTableData] = useState([]);
+
+    const setJsonTableData = (json) => {
+        let newArray = json.map((item, index) => ({index, ...item}));
+        for (var i = 0; i < newArray.length; i++) {
+            newArray[i].index++;
+        }
+        newArray.forEach((item) => {
+            item["quarter"] = fromFormat(item["quarter"]);
+        });
+        setTableData(newArray);
+        setTableVisibility(true);
+    }
+    return (
+        <Jumbotron>
+            <Container className="text-left">
+                <h1>Full Department Courses Summary</h1>
+                <FullCoursesForm setCourseJSON={setJsonTableData} fetchJSON={fetchFullCourses} onSubmit={() => { setTableVisibility(false) }} />
+            </Container>
+            <Container style={{ marginTop: "20px" }} className={"text-center"}>
+                {tableVisibility && (tableData.length ? <FullCourseTable data={tableData} /> : "There are no results!")}
+            </Container>
+        </Jumbotron>
+    );
+};
+
+export default FullDeptSummary;
