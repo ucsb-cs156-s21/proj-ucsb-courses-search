cf88016542449ea27d8c1677a49633fa176f343e
Steven Huynh-Tran <htransteven@gmail.com>
2021-02-23 17:07:07 -0800

    add rel='noopener noreferrer' to footer links

--- javascript/src/main/components/Footer/AppFooter.js
+++ javascript/src/main/components/Footer/AppFooter.js
@@ -4,17 +4,22 @@ const AppFooter = () => {
   return (
     <footer className="bg-light p-3 text-center">
       This app is a class project of{" "}
-      <a href="https://ucsb-cs156.github.io" target="_blank">
+      <a
+        href="https://ucsb-cs156.github.io"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         CMPSC 156
       </a>{" "}
       at{" "}
-      <a href="https://ucsb.edu" target="_blank">
+      <a href="https://ucsb.edu" target="_blank" rel="noopener noreferrer">
         UCSB
       </a>
       . Check out the source code on{" "}
       <a
         href="https://github.com/ucsb-cs156-w21/proj-ucsb-courses-search"
         target="_blank"
+        rel="noopener noreferrer"
       >
         GitHub
       </a>
