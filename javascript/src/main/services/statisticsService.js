eb64bd607d3bdb682528bdbb5921f9fee89c2ff6
dougyuan <59560618+dougyuan@users.noreply.github.com>
2021-02-25 01:27:15 -0800

    Update statisticsService.js

--- javascript/src/main/services/statisticsService.js
+++ javascript/src/main/services/statisticsService.js
@@ -2,15 +2,15 @@ import fetch from "isomorphic-unfetch";
 
 const fetchFullCourses = async (fields) => {
     const url = `/api/public/statistics/fullCoursesByDept?startQuarter=${encodeURIComponent(fields.startQuarter)}&endQuarter=${encodeURIComponent(fields.endQuarter)}&department=${encodeURIComponent(fields.department)}`;
-    const fullCoursesResponse = await fetch(url); 
+    const fullCoursesResponse = await fetch(url);
     return fullCoursesResponse.json();
 }
 
 const fetchDivisionOccupancy = async (fields) => {
     const url = `/api/public/statistics/courseOccupancyByDivision?startQuarter=${fields.startQuarter}&endQuarter=${fields.endQuarter}&department=${fields.department}&level=${fields.level}`;
-    const divisionOccupancyResponse = await fetch(url);   
+    const divisionOccupancyResponse = await fetch(url);
     return divisionOccupancyResponse.json();
-} 
+}
 
 const fetchClassSize = async (fields) => {
     const url = `/api/public/statistics/classSize?startQuarter=${encodeURIComponent(fields.startQuarter)}&endQuarter=${encodeURIComponent(fields.endQuarter)}`;
@@ -26,10 +26,9 @@ const fetchCourseOccupancy = async(fields) => {
 }
 
 const fetchFullDept = async(fields) => {
-    const url = `/api/public/statistics/fullDeptSummary?startQuarter=${encodeURIComponent(fields.startQuarter)}`;
-    const fullDeptResponse = await fetch(url);
-
-    return fullDeptResponse.json();
+    const url = `/api/public/statistics/totalCourses?quarter=${encodeURIComponent(fields.quarter)}`;
+    const response = await fetch(url);
+    return response.json();
 }
 
 export { fetchClassSize, fetchDivisionOccupancy, fetchCourseOccupancy, fetchFullCourses, fetchFullDept};
