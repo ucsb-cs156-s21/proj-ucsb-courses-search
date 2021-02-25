ef0c33594782a73e95037548152f2afe46f7e3da
dougyuan <59560618+dougyuan@users.noreply.github.com>
2021-02-25 00:25:42 -0800

    Update FullSummary.java

--- src/main/java/edu/ucsb/courses/documents/statistics/FullSummary.java
+++ src/main/java/edu/ucsb/courses/documents/statistics/FullSummary.java
@@ -52,7 +52,7 @@ public class FullSummary {
     }
 
     public void setNumFullCourses(String NumFullCourses) {
-        this.NumFullCourses = numFullCourses;
+        this.numFullCourses = numFullCourses;
     }
 
     @Override
