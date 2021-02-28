47568d171ee23b0cab9aacab37ef9cb890595951
codysasaki <cody_sasaki@umail.ucsb.edu>
2021-02-27 20:19:05 -0800

    cs - Added download CSV button to Ge search page

--- javascript/src/main/pages/History/Ge.js
+++ javascript/src/main/pages/History/Ge.js
@@ -4,6 +4,8 @@ import { Jumbotron } from "react-bootstrap";
 import BasicCourseTable from "main/components/BasicCourseSearch/BasicCourseTable";
 import GeCourseSearchForm from "main/components/BasicCourseSearch/GeCourseSearchForm";
 import {  fetchGeQtrJSON } from "main/services/courseSearches";
+import { CSVLink } from "react-csv";
+import { Button } from "react-bootstrap";
 
 const Ge = () => {
 
@@ -24,13 +26,18 @@ const Ge = () => {
     //const [courseJSON, setCourseJSON] = useState('{"course" : "cs148"}');
     // const courseJSON = '{"course" : "cs156"}';
     const [courseJSON, setCourseJSON] = useState(initialCourseJSON);
-
+    const courseHeaders = [
+        { label: "courseId", key: "courseId" },
+        { label: "title", key: "title" },
+        { label: "units", key: "unitsFixed" }
+    ]
     return (
         <Jumbotron>
             <div className="text-left">
                 <h2>Search Archived Course Data from MongoDB</h2>
                 <h5>Search GE Through Various Quarters</h5>
                 <GeCourseSearchForm setCourseJSON={setCourseJSON} fetchJSON={fetchGeQtrJSON} />
+                <Button><CSVLink style={{color: "white"}} headers={courseHeaders} data={courseJSON.classes} filename = {"CourseTable.csv"}>Download CSV</CSVLink></Button>
                 <BasicCourseTable classes={courseJSON.classes} />
             </div>
         </Jumbotron>
