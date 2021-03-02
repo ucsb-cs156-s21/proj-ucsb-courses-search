67b06c0bd8310fdf06df2349bcb781b723205051
codysasaki <cody_sasaki@umail.ucsb.edu>
2021-03-01 21:56:26 -0800

    cs - removed unnecessary code from courseSearches.js fetchGeQtrJSON

--- javascript/src/main/services/courseSearches.js
+++ javascript/src/main/services/courseSearches.js
@@ -16,10 +16,7 @@ const fetchGeQtrJSON = async (_event, fields) => {
     const url = `/api/public/history/gesearch?startQtr=${fields.startQuarter}&endQtr=${fields.endQuarter}&geCode=${fields.geCode}`;
     const courseResponse = await fetch(url);
     if (courseResponse.ok){
-        if(courseResponse.json().length == 0){
-            alert("Empty Results!");
-        }
-    return courseResponse.json();
+        return courseResponse.json();
     }
     else{
         alert("HTTP-Error: "+ courseResponse.status);
