db60d9be3b05d743e91a3158b7077320cf942f89
Derek Garcia <derekgarcia@ucsb.edu>
2021-03-02 17:40:58 -0800

    DG - Added Download CSV button to Insturctor Search

--- javascript/src/main/pages/History/Instructor.js
+++ javascript/src/main/pages/History/Instructor.js
@@ -4,6 +4,8 @@ import { Jumbotron } from "react-bootstrap";
 import BasicCourseTable from "main/components/BasicCourseSearch/BasicCourseTable";
 import CourseSearchFormInstructor from "main/components/BasicCourseSearch/CourseSearchFormInstructor";
 import {  fetchInstructorHistoryNameQtrJSON } from "main/services/courseSearches";
+import { CSVLink } from "react-csv";
+import { Button } from "react-bootstrap";
 
 const Instructor = () => {
 
@@ -16,12 +18,30 @@ const Instructor = () => {
 
     const [courseJSON, setCourseJSON] = useState(initialCourseJSON);
 
+
+    const courseHeaders = [
+        { label: "courseId", key: "courseId" },
+        { label: "title", key: "title" },
+        { label: "units", key: "unitsFixed" }
+    ]
+
     return (
         <Jumbotron>
             <div className="text-left">
                 <h2>Search Archived Course Data from MongoDB</h2>
                 <h5>Search Instructor Through Various Quarters</h5>
                 <CourseSearchFormInstructor setCourseJSON={setCourseJSON} fetchJSON={fetchInstructorHistoryNameQtrJSON} />
+
+                <Button style={{margin: "1rem 0"}}>
+                    <CSVLink    
+                        style={{color: "white"}}
+                        headers={courseHeaders} 
+                        data={courseJSON.classes} 
+                        filename = {"CourseTable.csv"}>
+                        Download CSV
+                    </CSVLink>
+                </Button>
+                
                 <BasicCourseTable classes={courseJSON.classes} />
                 
             </div>
