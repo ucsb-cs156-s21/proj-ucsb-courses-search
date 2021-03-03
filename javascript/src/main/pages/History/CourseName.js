1fc90d45d44e2a1d26cb3498aedaee6a1e6336fb
Andrew Tran <andrew_tran@ucsb.edu>
2021-03-02 17:48:47 -0800

    at - added download csv button to name search page

--- javascript/src/main/pages/History/CourseName.js
+++ javascript/src/main/pages/History/CourseName.js
@@ -4,6 +4,8 @@ import { Jumbotron } from "react-bootstrap";
 import BasicCourseTable from "main/components/BasicCourseSearch/BasicCourseTable";
 import CourseSearchCourseStartEndQtr from "main/components/BasicCourseSearch/CourseSearchCourseStartEndQtr";
 import { fetchCourseHistoryNameQtrJSON } from "main/services/courseSearches";
+import { CSVLink } from "react-csv";
+import { Button } from "react-bootstrap";
 
 const CourseName = () => {
     const initialCourseJSON = {
@@ -15,12 +17,20 @@ const CourseName = () => {
 
     const [courseJSON, setCourseJSON] = useState(initialCourseJSON);
 
+    // courseId, title, sectionNumber, instructor, enroll code, units, total enrolled students, max enrolled
+    const courseHeaders = [
+        { label: "courseId", key: "courseId" },
+        { label: "title", key: "title" },
+        { label: "units", key: "unitsFixed" }
+    ]
+
     return (
         <Jumbotron>
             <div className="text-left">
                 <h2>Search Archived Course Data from MongoDB</h2>
                 <h5>Search By Course Name Through Various Quarters</h5>
                 <CourseSearchCourseStartEndQtr setCourseJSON={setCourseJSON} fetchJSON={fetchCourseHistoryNameQtrJSON} />
+                <Button><CSVLink style={{color: "white"}} headers={courseHeaders} data={courseJSON.classes} filename = {"CourseTable.csv"}>Download CSV</CSVLink></Button>
                 <BasicCourseTable classes={courseJSON.classes} />
             </div>
         </Jumbotron>
