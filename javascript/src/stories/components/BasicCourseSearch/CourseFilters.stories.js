b76daa7deddd9cf48c03d54d1a70edf81bac9bbf
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-03-05 16:11:06 -0800

    pc - add code to story to illustrate how to use the component

--- javascript/src/stories/components/BasicCourseSearch/CourseFilters.stories.js
+++ javascript/src/stories/components/BasicCourseSearch/CourseFilters.stories.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { action } from "@storybook/addon-actions";
 
 import CourseFilters from "main/components/BasicCourseSearch/CourseFilters";
 export default {
   title: 'components/BasicCourseSearch/CourseFilters',
-  component: CourseFilters
+  component: CourseFilters,
 };
 
-const Template = (args) => <CourseFilters {...args} />;
+const Template = (_args) => {
+  const [closed, setClosed] = useState(false);
+  const [full, setFull] = useState(false);
+  const [cancelled, setCancelled] = useState(false);
+
+  const toggleIt = (stateVar, setStateVar, label) => {
+    const newValue = !stateVar
+    console.log(`toggling ${label} from ${stateVar} to ${newValue}`);
+    action(`toggling ${label} from ${stateVar} to ${newValue}`);
+    setStateVar(newValue);
+  }
+  
+  return (
+    <CourseFilters 
+      cancelled={cancelled}
+      handleCancelledOnChange={()=>toggleIt(cancelled,setCancelled,"cancelled")}
+      full={full}
+      handleFullOnChange={()=>toggleIt(full,setFull,"full")}
+      closed={closed}
+      handleClosedOnChange={()=>toggleIt(closed,setClosed,"closed")}
+      {..._args}
+       />
+  );
+}
 
 export const AllUnchecked = Template.bind({});
-AllUnchecked.args = {
+AllUnchecked._args = {
 };
 
