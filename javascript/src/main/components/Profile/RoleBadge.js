232ed3ed0d93ea0afe392d582032463c02abe864
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-10 19:21:15 -0700

    pc - fix spacing per code review

--- javascript/src/main/components/Profile/RoleBadge.js
+++ javascript/src/main/components/Profile/RoleBadge.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Badge } from "react-bootstrap";
 
 export default function RoleBadge(props) {
-    const {roleInfo} = props;
+  const { roleInfo } = props;
   return (
-     roleInfo ?
-        <Badge variant="primary">{roleInfo.role}</Badge> 
-        :
-        <span>Loading role...</span>
+    roleInfo ?
+      <Badge variant="primary">{roleInfo.role}</Badge> :
+      <span>Loading role...</span>
   );
 }
