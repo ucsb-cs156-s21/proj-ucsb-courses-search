78324c1e507400ce621eeea85d4803083799e8f6
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-24 21:19:03 -0700

    pc - fix failing tests and remove commented out code

--- javascript/src/main/components/BasicCourseSearch/CourseSearchFormInstructor.js
+++ javascript/src/main/components/BasicCourseSearch/CourseSearchFormInstructor.js
@@ -23,14 +23,6 @@ const CourseSearchFormInstructor = ({ setCourseJSON, fetchJSON }) => {
         });
     };
 
-    // const handleStartQuarterOnChange = (event) => {
-    //     setStartQuarter(event.target.value);
-    // };
-
-    // const handleEndQuarterOnChange = (event) => {
-    //     setEndQuarter(event.target.value);
-    // };
-
     const handleInstructorOnChange = (event) => {
         setInstructor(event.target.value);
     };
@@ -45,18 +37,6 @@ const CourseSearchFormInstructor = ({ setCourseJSON, fetchJSON }) => {
                 label={"Start Quarter"}
             />
 
-            {/* <Form.Group controlId="InstructorSearch.StartController">
-                <Form.Label>Start Quarter</Form.Label>
-                <Form.Control as="select" onChange={handleStartQuarterOnChange} value={startQuarter}  >
-                    <option value="20212">S21</option>
-                    <option value="20211">W21</option>
-                    <option value="20204">F20</option>
-                    <option value="20203">M20</option>
-                    <option value="20202">S20</option>
-                    <option value="20201">W20</option>
-                </Form.Control>
-            </Form.Group> */}
-
             <SelectQuarter
                 quarters={quarters}
                 quarter={endQuarter}
@@ -65,17 +45,6 @@ const CourseSearchFormInstructor = ({ setCourseJSON, fetchJSON }) => {
                 label={"End Quarter"}
             />
 
-            {/* <Form.Group controlId="Instructor.EndController">
-                <Form.Label>End Quarter</Form.Label>
-                <Form.Control as="select" onChange={handleEndQuarterOnChange} value={endQuarter}  >
-                    <option value="20212">S21</option>
-                    <option value="20211">W21</option>
-                    <option value="20204">F20</option>
-                    <option value="20203">M20</option>
-                    <option value="20202">S20</option>
-                    <option value="20201">W20</option>
-                </Form.Control>
-            </Form.Group> */}
             <Form.Group controlId="InstructorSearch.Instructor">
                 <Form.Label>Instructor</Form.Label>
                 <Form.Control type="text" onChange={handleInstructorOnChange} value={instructorText} placeholder="Instructor Last Name">
--- javascript/src/test/components/BasicCourseSearch/CourseSearchFormInstructor.test.js
+++ javascript/src/test/components/BasicCourseSearch/CourseSearchFormInstructor.test.js
@@ -42,54 +42,54 @@ test("when I select a end quarter, the state for the end quarter changes", () =>
     expect(instructorInputs.value).toBe("KHARITONOVA");
 });
 
-// test("when I click submit, I get back the information about a specified instructor between certain quarters", async () => {
+test("when I click submit, I get back the information about a specified instructor between certain quarters", async () => {
 
-//   const sampleReturnValue = {
-//       "sampleKey": "sampleValue"
-//   };
+  const sampleReturnValue = {
+      "sampleKey": "sampleValue"
+  };
 
-//   // Create spy functions (aka jest function, magic function)
-//   // The function doesn't have any implementation unless
-//   // we specify one.  But it does keep track of whether 
-//   // it was called, how many times it was called,
-//   // and what it was passed.
+  // Create spy functions (aka jest function, magic function)
+  // The function doesn't have any implementation unless
+  // we specify one.  But it does keep track of whether 
+  // it was called, how many times it was called,
+  // and what it was passed.
 
-//   const setCourseJSONSpy = jest.fn();
-//   const fetchJSONSpy = jest.fn();
+  const setCourseJSONSpy = jest.fn();
+  const fetchJSONSpy = jest.fn();
 
-//   fetchJSONSpy.mockResolvedValue(sampleReturnValue);
+  fetchJSONSpy.mockResolvedValue(sampleReturnValue);
 
-//   const { getByText, getByLabelText } = render(
-//       <CourseSearchFormInstructor setCourseJSON={setCourseJSONSpy} fetchJSON={fetchJSONSpy} />
-//   );
+  const { getByText, getByLabelText } = render(
+      <CourseSearchFormInstructor setCourseJSON={setCourseJSONSpy} fetchJSON={fetchJSONSpy} />
+  );
 
-//   const expectedFields = {
-//       startQuarter: "20204",
-//       endQuarter: "20204",
-//       instructorText: "KHARITONOVA"
-//   };
+  const expectedFields = {
+      startQuarter: "20204",
+      endQuarter: "20204",
+      instructorText: "KHARITONOVA"
+  };
 
-//   const selectStartQuarter = getByLabelText("Start Quarter")
-//   userEvent.selectOptions(selectStartQuarter, "20204");
-//   const selectEndQuarter = getByLabelText("End Quarter")
-//   userEvent.selectOptions(selectEndQuarter, "20204");
-//   const selectInstructor= getByLabelText("Instructor")
-//   userEvent.type(selectInstructor, "KHARITONOVA");
+  const selectStartQuarter = getByLabelText("Start Quarter")
+  userEvent.selectOptions(selectStartQuarter, "20204");
+  const selectEndQuarter = getByLabelText("End Quarter")
+  userEvent.selectOptions(selectEndQuarter, "20204");
+  const selectInstructor= getByLabelText("Instructor")
+  userEvent.type(selectInstructor, "KHARITONOVA");
 
 
-//   const submitButton = getByText("Submit");
-//   userEvent.click(submitButton);
+  const submitButton = getByText("Submit");
+  userEvent.click(submitButton);
 
-//   // we need to be careful not to assert this expectation
-//   // until all of the async promises are resolved
-//   await waitFor(() => expect(setCourseJSONSpy).toHaveBeenCalledTimes(1));
-//   await waitFor(() => expect(fetchJSONSpy).toHaveBeenCalledTimes(1));
+  // we need to be careful not to assert this expectation
+  // until all of the async promises are resolved
+  await waitFor(() => expect(setCourseJSONSpy).toHaveBeenCalledTimes(1));
+  await waitFor(() => expect(fetchJSONSpy).toHaveBeenCalledTimes(1));
 
-//   // assert that ourSpy was called with the right value
-//   expect(setCourseJSONSpy).toHaveBeenCalledWith(sampleReturnValue);
-//   expect(fetchJSONSpy).toHaveBeenCalledWith(expect.any(Object), expectedFields);
+  // assert that ourSpy was called with the right value
+  expect(setCourseJSONSpy).toHaveBeenCalledWith(sampleReturnValue);
+  expect(fetchJSONSpy).toHaveBeenCalledWith(expect.any(Object), expectedFields);
 
-// });
+});
 
 test("when I click submit with an EMPTY JSON, setcourseJSON is not called", async () => {
 
