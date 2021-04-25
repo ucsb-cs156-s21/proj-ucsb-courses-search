94bae22d421a46238657a3506b978c313f9664a5
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-04-24 18:01:03 -0700

    pc - add required credits to footer

--- javascript/src/test/components/Footer/AppFooter.test.js
+++ javascript/src/test/components/Footer/AppFooter.test.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import AppFooter from "main/components/Footer/AppFooter";
+import AppFooter, {space} from "main/components/Footer/AppFooter";
 
 describe("AppFooter tests", () => {
+
+  test("space stands for a space", () => {
+    expect(space).toBe(" ");
+  });
+
+
   test("renders without crashing", () => {
     render(<AppFooter />);
   });
@@ -17,9 +23,9 @@ describe("AppFooter tests", () => {
       "href",
       "https://ucsb.edu"
     );
-    expect(getByText("GitHub").closest("a")).toHaveAttribute(
+    expect(getByText(/GitHub/i).closest("a")).toHaveAttribute(
       "href",
-      "https://github.com/ucsb-cs156-w21/proj-ucsb-courses-search"
+      "https://github.com/ucsb-cs156-s21/proj-ucsb-courses-search"
     );
   });
 });
