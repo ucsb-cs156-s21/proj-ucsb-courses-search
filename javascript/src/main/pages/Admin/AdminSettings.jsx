d9f27b7a2a219da6b84731e832aad51a17c6e809
Evan Yip <etyip@ucsb.edu>
2021-05-11 17:48:33 -0700

    ey - added period and changed front end test to reflect changes

--- javascript/src/main/pages/Admin/AdminSettings.jsx
+++ javascript/src/main/pages/Admin/AdminSettings.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AdminSettings = () => {
-  return <div>This page will be available to admins to select which quarters are available across the app for searching, statistics, and scheduling. It is currently in progress</div>;
+  return <div>This page will be available to admins to select which quarters are available across the app for searching, statistics, and scheduling. It is currently in progress.</div>;
 };
 
 export default AdminSettings;
