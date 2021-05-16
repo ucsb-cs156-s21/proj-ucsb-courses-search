192eca853c6faa7b423db9113a3619125766a373
Thao Phan <thaophan000@gmail.com>
2021-05-15 22:32:15 -0700

    tp - Home page and Basic Search page no longer has a hard coded form of only 3 quarters, it now uses QuarterSelector to display all quarters from Fall 2008 - Fall 2021

--- javascript/src/main/components/BasicCourseSearch/CourseSearchFormQtrDeptOnly.js
+++ javascript/src/main/components/BasicCourseSearch/CourseSearchFormQtrDeptOnly.js
@@ -1,52 +1,60 @@
 import React, { useState } from "react";
-import { Form, Button} from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import { useToasts } from "react-toast-notifications";
+import { quarterRange } from "main/utils/quarterUtilities";
+import SelectQuarter from "main/components/BasicCourseSearch/SelectQuarter";
 
 const CourseSearchFormQtrDeptOnly = ({ setCourseJSON, fetchJSON }) => {
-    const [quarter, setQuarter] = useState("20212");
-    const [department, setDepartment] = useState("CMPSC");
-    const { addToast } = useToasts()
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        fetchJSON(event, { quarter, department }).then((courseJSON) => {
-            if(courseJSON.total === 0){
-                addToast("There are no courses that match the requested criteria.", { appearance: "error" });
-            }
-            setCourseJSON(courseJSON);
-        });
-    };
+	const quarters = quarterRange("20081", "20211");
+	const [quarter, setQuarter] = useState("20212");
+	const [department, setDepartment] = useState("CMPSC");
+	const { addToast } = useToasts();
 
-    const handleQuarterOnChange = (event) => {
-        setQuarter(event.target.value);
-    };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		fetchJSON(event, { quarter, department }).then((courseJSON) => {
+			if (courseJSON.total === 0) {
+				addToast("There are no courses that match the requested criteria.", {
+					appearance: "error",
+				});
+			}
+			setCourseJSON(courseJSON);
+		});
+	};
 
-    const handleDepartmentOnChange = (event) => {
-        setDepartment(event.target.value);
-    };
+	const handleQuarterOnChange = (event) => {
+		setQuarter(event.target.value);
+	};
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="BasicSearch.Quarter">
-                <Form.Label>Quarter</Form.Label>
-                <Form.Control as="select" onChange={handleQuarterOnChange} value={quarter}  >
-                    <option value="20212">S21</option>
-                    <option value="20211">W21</option>
-                    <option value="20204">F20</option>
-                </Form.Control>
-            </Form.Group>
-            <Form.Group controlId="BasicSearch.Department">
-                <Form.Label>Department</Form.Label>
-                <Form.Control as="select" onChange={handleDepartmentOnChange} value={department}>
-                    <option>CMPSC</option>
-                    <option>MATH</option>
-                </Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-    );
+	const handleDepartmentOnChange = (event) => {
+		setDepartment(event.target.value);
+	};
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<SelectQuarter
+				quarters={quarters}
+				quarter={quarter}
+				setQuarter={handleQuarterOnChange}
+				controlId={"BasicSearch.Quarter"}
+				label={"Quarter"}
+			/>
+			<Form.Group controlId="BasicSearch.Department">
+				<Form.Label>Department</Form.Label>
+				<Form.Control
+					as="select"
+					onChange={handleDepartmentOnChange}
+					value={department}
+				>
+					<option>CMPSC</option>
+					<option>MATH</option>
+				</Form.Control>
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Submit
+			</Button>
+		</Form>
+	);
 };
 
 export default CourseSearchFormQtrDeptOnly;
