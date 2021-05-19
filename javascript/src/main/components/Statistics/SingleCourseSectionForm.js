57cd3d0e28c9c70165d974e3dd26e0127517d460
Sheldin <sheldinlau@cs.ucsb.edu>
2021-05-19 15:58:31 -0700

    KH EY SL - added the form to get the user input for SingleCourseSearch

--- javascript/src/main/components/Statistics/SingleCourseSectionForm.js
+++ javascript/src/main/components/Statistics/SingleCourseSectionForm.js
@@ -4,24 +4,42 @@ import DepartmentFormSelect from "main/components/Statistics/DepartmentFormSelec
 import QuarterFormSelect from "main/components/Statistics/QuarterFormSelect";
 
 const SingleCourseSectionForm = ({ setCourseJSON, fetchJSON, onSubmit = () => {} }) => {
-
-    // const [quarter, setQuarter] = useState("20204");
     const [startQuarter, setStartQuarter] = useState("20204");
     const [endQuarter, setEndQuarter] = useState("20211");
     const [department, setDepartment] = useState("CMPSC");
     const [loading, setLoading] = useState(false);
+    const [courseNumber, setCourseNumber] = useState("");
+    const [courseSuf, setCourseSuf] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
         onSubmit();
         setLoading(true);
-        fetchJSON({startQuarter, endQuarter, department})
+        fetchJSON({startQuarter, endQuarter, department, courseNumber, courseSuf})
         .then((courseJSON)=> {
-            setOccupancyJson(courseJSON);
+            console.log(courseJSON);
+            setCourseJSON(courseJSON);
             setLoading(false);
         });
     };
 
+    const handleCourseNumberOnChange = (event) => {
+        const rawCourse = event.target.value;
+        if (rawCourse.match(/\d+/g) != null) {
+            const number = rawCourse.match(/\d+/g)[0];
+            setCourseNumber(number);
+        } else {
+            setCourseNumber("");
+        }
+        
+        if (rawCourse.match(/[a-zA-Z]+/g) != null) {
+            const suffix = rawCourse.match(/[a-zA-Z]+/g)[0];
+            setCourseSuf(suffix);
+        } else {
+            setCourseSuf("");
+        }
+    };
+    
     return (
         <Form onSubmit={handleSubmit}>
             <Form.Group controlId="SingleCourseSection.StartQuarter">
@@ -35,6 +53,10 @@ const SingleCourseSectionForm = ({ setCourseJSON, fetchJSON, onSubmit = () => {}
             <Form.Group controlId="SingleCourseSection.Department">
                 <Form.Label>Department</Form.Label>
                 <DepartmentFormSelect handleSelect={setDepartment} value={department}/>
+            </Form.Group>
+            <Form.Group controlId="CourseNameSearch.CourseNumber">
+                <Form.Label>Course Number (Try searching '16' or '130A')</Form.Label>
+                <Form.Control onChange={handleCourseNumberOnChange} defaultValue={courseNumber} />
             </Form.Group>
             <Button variant="primary" type="submit" className={"text-center"} disabled={loading}>
                 Submit
