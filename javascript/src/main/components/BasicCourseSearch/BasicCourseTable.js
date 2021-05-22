0c32ed2ac77f9fe837419aeff475995fb80120ec
sriya112 <sriyaaluru@gmail.com>
2021-05-22 14:15:15 -0400

    sa/co small changes to fix type issue from earlier

--- javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
+++ javascript/src/main/components/BasicCourseSearch/BasicCourseTable.js
@@ -55,12 +55,15 @@ const BasicCourseTable = ({ classes, checks, displayQuarter, allowExport }) => {
   }
   const renderSectionTimes = (_cell, row) => {
     if (row.timeLocations.length > 0) {
-      const startHours = parseInt(row.timeLocations[0].beginTime.substring(0, 2));
-      const endHours = parseInt(row.timeLocations[0].endTime.substring(0, 2));
+      const startHours = (row.timeLocations[0].beginTime.substring(0, 1) != "0") ? parseInt(row.timeLocations[0].beginTime.substring(0, 2)) : parseInt(row.timeLocations[0].beginTime.substring(1, 2));
+      const endHours = (row.timeLocations[0].endTime.substring(0, 1) != "0") ? parseInt(row.timeLocations[0].endTime.substring(0, 2)) : parseInt(row.timeLocations[0].endTime.substring(1, 2));
       const startDisplayHours = (startHours % 12);
       const endDisplayHours = (endHours % 12);
       const timeTypeStart = (startHours > 12) ? ("PM") : ("AM");
       const timeTypeEnd = (endHours > 12) ? ("PM") : ("AM");
+      // if(startDisplayHours < 10) {
+      //   const
+      // }
       return ((toString(startDisplayHours) + row.timeLocations[0].beginTime.substring(2) + timeTypeStart + " - " + toString(endDisplayHours) + row.timeLocations[0].endTime.substring(2) + timeTypeEnd))
     }
     return ("TBD")
