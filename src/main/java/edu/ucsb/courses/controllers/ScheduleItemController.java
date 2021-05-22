50dd70b34a67d3cf6e9b38cd3f23d47a4bed63fb
Alan Zhang <alan.zhang.052@gmail.com>
2021-05-22 14:26:13 -0700

    az - Removed commented-out lines

--- src/main/java/edu/ucsb/courses/controllers/ScheduleItemController.java
+++ src/main/java/edu/ucsb/courses/controllers/ScheduleItemController.java
@@ -89,45 +89,6 @@ public class ScheduleItemController {
         return ResponseEntity.notFound().build();
     }
 
-    // @GetMapping(value = "/getByScheduleId", produces = "application/json")
-    // public ResponseEntity<String> getScheduleItemsByScheduleId(@RequestHeader("Authorization") String authorization, 
-    //                                                            //@RequestParam Long id,
-    //                                                            @RequestParam Long scheduleId) throws JsonProcessingException {
-    //     if(!authControllerAdvice.getIsMember(authorization)){
-    //         return new ResponseEntity<>("Unauthorized Request", HttpStatus.UNAUTHORIZED);
-    //     }
-    //     AppUser appUser = authControllerAdvice.getUser(authorization);
-    //     List<ScheduleItem> savedSched = scheduleItemRepository.findByScheduleId(scheduleId);
-    //     Optional<Schedule> sched = scheduleRepository.findById(scheduleId);
-    //     if (sched.isEmpty()){
-    //         return ResponseEntity.badRequest().build();
-    //     }
-    //     String res = "";
-    //     for (ScheduleItem item: savedSched){
-    //         if (item.getAppUser().equals(appUser)) {
-    //             Optional<Course> course = archivedCourseRepository.findOneByQuarterAndCourseId(sched.get().getQuarter(), String.valueOf(item.getId()));
-    //             if (course.isEmpty()){
-    //                 return ResponseEntity.noContent().build();
-    //             }
-    //             res = res.concat("{courseId= '"+course.get().getCourseId()+"', ");
-    //             res = res.concat("title= '"+course.get().getTitle()+"', ");
-
-    //             for(Section section : course.get().getClassSections()){
-    //                 if(item.getDiscussionCode().equals(section.getEnrollCode())){
-    //                     TimeLocation tl = section.getTimeLocations().get(0);
-    //                     res = res.concat("days= '"+tl.getDays()+"', ");
-    //                     res = res.concat("beginTime= '"+tl.getBeginTime()+"', ");
-    //                     res = res.concat("endTime= '"+tl.getEndTime()+"'}");
-    //                 }
-    //             }
-    //             res = res.concat("!");
-    //         }
-    //         else{return ResponseEntity.badRequest().build();}
-    //     }
-    //     if (res.length() == 0){return ResponseEntity.noContent().build();}
-    //     return ResponseEntity.ok().body(res.substring(0,res.length()-1));
-    // }
-
     @GetMapping(value = "/get", produces = "application/json")
     public ResponseEntity<String> getScheduleItemById(@RequestHeader("Authorization") String authorization,
                                                       @RequestParam Long id) throws JsonProcessingException {
@@ -143,29 +104,5 @@ public class ScheduleItemController {
 
         return ResponseEntity.badRequest().build();
     }
-
-
-    // @DeleteMapping(value = "/deleteByScheduleId", produces = "application/json")
-    // public ResponseEntity<String> removeScheduleItemsByScheduleId(@RequestHeader("Authorization") String authorization,
-    //                                                               @RequestParam Long scheduleId){
-    //     AppUser appUser = authControllerAdvice.getUser(authorization);
-    //     if(!authControllerAdvice.getIsMember(authorization)){
-    //         return new ResponseEntity<>("Unauthorized Request", HttpStatus.UNAUTHORIZED);
-    //     }
-    //     List<ScheduleItem> savedSched = scheduleItemRepository.findByScheduleId(scheduleId);
-    //     boolean verified = true;
-    //     for (ScheduleItem item: savedSched){
-    //         if (!item.getAppUser().equals(appUser)){
-    //             verified = false;
-    //             break;
-    //         }
-    //     }
-    //     if (verified) {
-    //         scheduleItemRepository.deleteByScheduleId(scheduleId);
-    //         return ResponseEntity.ok().build();
-    //     }
-    //     return ResponseEntity.badRequest().build();
-    // }
-
 
 }
