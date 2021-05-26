41c810faa8b9109fa999e83ef6bc064bd1739f6a
Alan Zhang <alan.zhang.052@gmail.com>
2021-05-25 20:40:02 -0700

    lw/az - Created AddToSchedule components to create new page

---
+++ javascript/src/main/pages/Schedule/AddToSchedule.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useEffect } from "react";
+import { Jumbotron } from "react-bootstrap";
+import AddToScheduleTable from "main/components/Schedule/AddToScheduleTable";
+
+import { useAuth0 } from "@auth0/auth0-react";
+import useSWR from "swr";
+import { fetchWithToken } from "main/utils/fetch";
+import {
+  buildDeleteSchedule
+} from "main/services/Schedule/scheduleServices";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { useToasts } from 'react-toast-notifications'
+
+const AddToSchedule = () => {
+  const { addToast } = useToasts();
+  const { getAccessTokenSilently: getToken} = useAuth0();
+  const { data: schedules, mutate: mutateSchedules } = useSWR(["/api/member/schedule/getSchedules", getToken], fetchWithToken);
+  const history = useHistory();
+
+  useEffect(() => {
+    mutateSchedules();
+  }, [mutateSchedules]);
+
+  const deleteSchedule = buildDeleteSchedule(
+    getToken,
+    (response) => {
+      if (response.error) {
+        addToast(response.error, { appearance: 'error' });
+      }
+      else {
+        mutateSchedules();
+        addToast("Schedule deleted", { appearance: 'success' });
+      }
+    },
+    (_err) => {
+      addToast("Error deleting schedule", { appearance: 'error' });
+    }
+  );
+
+  return (
+    <Jumbotron>
+      <p>
+      The personal schedule page is intended as a place where students, faculty or staff can plan out a schedule of courses for a particular quarter.
+      </p>
+      <Button
+          data-testid={`new-schedule-button`}
+          onClick={() => history.push("/schedule/new")}
+        >
+          New Schedule
+        </Button>
+      {schedules && (<AddToScheduleTable data={schedules} deleteSchedule={deleteSchedule}/>)}
+
+    </Jumbotron>
+  );
+};
+
+export default AddToSchedule;
