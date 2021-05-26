c6ce5ce83ab4c6bf683c1dce7e430a2df2ee42df
Sydney Lim <sydneylim@ucsb.edu>
2021-05-26 13:14:51 -0700

    sl - undid changes to the prettycard files

--- javascript/src/test/components/Utilities/JSONPrettyCard.test.js
+++ javascript/src/test/components/Utilities/JSONPrettyCard.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import userEvent from "@testing-library/user-event";
 import { render } from "@testing-library/react";
 import JSONPrettyCard from "main/components/Utilities/JSONPrettyCard";
 
@@ -12,4 +13,31 @@ describe("JSONPrettyCard render tests", () => {
     );
   });
 
+  test("initial state of accordion is collapsed", () => {
+    const { getByTestId } = render(
+      <JSONPrettyCard expression={mockExpression} value={mockJSONValue} />
+    );
+    const collapseContainer = getByTestId(
+      `JSONPrettyPanel-${mockExpression}-collapse`
+    );
+    expect(collapseContainer.className).toEqual(
+      expect.not.stringContaining("show")
+    );
+  });
+
+  test("accordion shows collapsed content on click", async () => {
+    const { getByTestId } = render(
+      <JSONPrettyCard expression={mockExpression} value={mockJSONValue} />
+    );
+    const accordionToggle = getByTestId(
+      `JSONPrettyPanel-${mockExpression}-toggle`
+    );
+    userEvent.click(accordionToggle);
+
+    await new Promise((r) => setTimeout(r, 500));
+
+    expect(
+      getByTestId(`JSONPrettyPanel-${mockExpression}-collapse`).className
+    ).toEqual(expect.stringContaining("show"));
+  });
 });
