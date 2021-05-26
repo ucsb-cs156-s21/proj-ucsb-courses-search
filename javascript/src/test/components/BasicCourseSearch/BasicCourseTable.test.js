d39d462f3aae6027d84caebf62c353651ec2a325
Christine Ong <christine_ong@ucsb.edu>
2021-05-25 19:27:30 -0700

    fixed tests

--- javascript/src/test/components/BasicCourseSearch/BasicCourseTable.test.js
+++ javascript/src/test/components/BasicCourseSearch/BasicCourseTable.test.js
@@ -106,7 +106,7 @@ describe("BasicCourseTable tests", () => {
       isAuthenticated: true,
     });
     const {queryByText} = render(<BasicCourseTable classes = {courseFixtures.classesLectureOnly} />);
-    expect( queryByText("09:30AM - 10:45AM")).not.toBe(null);
+    expect( queryByText("9:30AM - 10:45AM")).not.toBe(null);
   });
 
 
@@ -204,7 +204,7 @@ describe("BasicCourseTable tests", () => {
       isAuthenticated: true,
     });
     const {queryByText} = render(<BasicCourseTable classes = {courseFixtures.classesSectionOnly} />);
-    expect( queryByText("09:00AM - 09:50AM")).not.toBe(null);
+    expect( queryByText("9:00AM - 09:50AM")).not.toBe(null);
   });
 
  test("check that sections times and days appear as TBD when they don't exist", () => {
