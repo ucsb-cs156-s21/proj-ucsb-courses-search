78f445a3d01f661df2d92a05338a5c3113bc0fe0
Sydney Lim <sydneylim@ucsb.edu>
2021-05-25 20:49:07 -0700

    sl - removed unnecessary tests and deleted the previously commented out JSONPrettyCard code

--- javascript/src/test/components/Utilities/JSONPrettyCard.test.js
+++ javascript/src/test/components/Utilities/JSONPrettyCard.test.js
@@ -1,43 +1,15 @@
-// import React from "react";
-// import userEvent from "@testing-library/user-event";
-// import { render } from "@testing-library/react";
-// import JSONPrettyCard from "main/components/Utilities/JSONPrettyCard";
+import React from "react";
+import { render } from "@testing-library/react";
+import JSONPrettyCard from "main/components/Utilities/JSONPrettyCard";
 
-// const mockExpression = "courseJSON";
-// const mockJSONValue = '{"course" : "cs148", "otherKey" : "other value"}';
+const mockExpression = "courseJSON";
+const mockJSONValue = '{"course" : "cs148", "otherKey" : "other value"}';
 
-// describe("JSONPrettyCard render tests", () => {
-//   test("component renders without crashing", () => {
-//     render(
-//       <JSONPrettyCard expression={mockExpression} value={mockJSONValue} />
-//     );
-//   });
+describe("JSONPrettyCard render tests", () => {
+  test("component renders without crashing", () => {
+    render(
+      <JSONPrettyCard expression={mockExpression} value={mockJSONValue} />
+    );
+  });
 
-//   test("initial state of accordion is collapsed", () => {
-//     const { getByTestId } = render(
-//       <JSONPrettyCard expression={mockExpression} value={mockJSONValue} />
-//     );
-//     const collapseContainer = getByTestId(
-//       `JSONPrettyPanel-${mockExpression}-collapse`
-//     );
-//     expect(collapseContainer.className).toEqual(
-//       expect.not.stringContaining("show")
-//     );
-//   });
-
-//   test("accordion shows collapsed content on click", async () => {
-//     const { getByTestId } = render(
-//       <JSONPrettyCard expression={mockExpression} value={mockJSONValue} />
-//     );
-//     const accordionToggle = getByTestId(
-//       `JSONPrettyPanel-${mockExpression}-toggle`
-//     );
-//     userEvent.click(accordionToggle);
-
-//     await new Promise((r) => setTimeout(r, 500));
-
-//     expect(
-//       getByTestId(`JSONPrettyPanel-${mockExpression}-collapse`).className
-//     ).toEqual(expect.stringContaining("show"));
-//   });
-// });
+});
