b87a9eca4744b48b6b0662ea60a2973fe4743332
Evan Yip <etyip@ucsb.edu>
2021-05-26 15:15:19 -0700

    ey - removing modified file from pull request

--- javascript/src/main/components/Schedule/AddSchedForm.js
+++ javascript/src/main/components/Schedule/AddSchedForm.js
@@ -1,93 +1,70 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import SelectQuarter from 'main/components/BasicCourseSearch/SelectQuarter';
-import { quarterRange } from 'main/utils/quarterUtilities';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const AddSchedForm = ({ createSchedule, updateSchedule, existingSchedule }) => {
-  const emptySchedule = {
-    name: '',
-    description: '',
-    quarter: '',
-    userId: '',
-  };
 
-  const [schedule, setSchedule] = useState(existingSchedule || emptySchedule);
-  const quarters = quarterRange('20084', '20214');
-  const localQuarter = localStorage.getItem('PersonalSchedule.Quarter');
-  const [quarter, setQuarter] = useState(
-    (existingSchedule ? existingSchedule.quarter : null) ||
-      localQuarter ||
-      quarters[0].yyyyq
-  );
+    const emptySchedule = {
+        name: "",
+        description:"",
+        quarter:"",
+        userId:""
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (createSchedule) {
-      createSchedule(schedule);
-    } else {
-      updateSchedule(schedule, schedule.id);
+    const [schedule, setSchedule] = useState(existingSchedule || emptySchedule);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (createSchedule) {
+            createSchedule(schedule);
+        }
+        else{
+            updateSchedule(schedule, schedule.id);
+        }
+    };
+
+    const handleNameOnChange = (event) => {
+        setSchedule({
+            ...schedule,
+            name: event.target.value
+          });
     }
-  };
 
-  const handleNameOnChange = (event) => {
-    setSchedule({
-      ...schedule,
-      name: event.target.value,
-    });
-  };
+    const handleDescriptionOnChange = (event) => {
+        setSchedule({
+            ...schedule,
+            description: event.target.value
+          });
+    }
 
-  const handleDescriptionOnChange = (event) => {
-    setSchedule({
-      ...schedule,
-      description: event.target.value,
-    });
-  };
+    const handleQuarterOnChange = (event) => {
+        setSchedule({
+          ...schedule,
+          quarter: event.target.value
+        });
+    }
 
-  const handleQuarterOnChange = (quarter) => {
-    localStorage.setItem('PersonalSchedule.Quarter', quarter);
-    setQuarter(quarter);
-    setSchedule({
-      ...schedule,
-      quarter: quarter,
-    });
-  };
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formsSchedName">
+                <Form.Label>Schedule Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Schedule Name" value={schedule.name} onChange={handleNameOnChange} data-testid="schedule-name" />
+            </Form.Group>
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formsSchedName">
-        <Form.Label>Schedule Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter Schedule Name"
-          value={schedule.name}
-          onChange={handleNameOnChange}
-          data-testid="schedule-name"
-        />
-      </Form.Group>
+            <Form.Group controlId="formSchedDes">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" placeholder="Enter Schedule Description" value={schedule.description} onChange={handleDescriptionOnChange} data-testid="schedule-description" />
+            </Form.Group>
 
-      <Form.Group controlId="formSchedDes">
-        <Form.Label>Description</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Enter Schedule Description"
-          value={schedule.description}
-          onChange={handleDescriptionOnChange}
-          data-testid="schedule-description"
-        />
-      </Form.Group>
-      <SelectQuarter
-        quarters={quarters}
-        quarter={quarter}
-        setQuarter={handleQuarterOnChange}
-        controlId={'PersonalSchedule.Quarter'}
-        label={'Quarter'}
-      />
+            <Form.Group controlId="formQuarter">
+                <Form.Label>Quarter</Form.Label>
+                <Form.Control type="text" placeholder="Enter Schedule Quarter" value={schedule.quarter} onChange={handleQuarterOnChange} data-testid="schedule-quarter" />
+            </Form.Group>
 
-      <Button variant="primary" type="submit" data-testid="schedule-submit">
-        Submit
-      </Button>
-    </Form>
-  );
+            <Button variant="primary" type="submit" data-testid="schedule-submit">
+                Submit
+            </Button>
+        </Form>
+    );
 };
 
 export default AddSchedForm;
--- javascript/src/test/components/Schedule/AddSchedForm.test.js
+++ javascript/src/test/components/Schedule/AddSchedForm.test.js
@@ -1,84 +1,86 @@
-import React from 'react';
-import { render, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import AddSchedForm from 'main/components/Schedule/AddSchedForm';
-jest.mock('isomorphic-unfetch');
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import AddSchedForm from "main/components/Schedule/AddSchedForm";
+jest.mock("isomorphic-unfetch");
 
-describe('AddSchedForm tests', () => {
-  test('renders without crashing', () => {
-    render(<AddSchedForm />);
-  });
 
-  test('when I type a name, the state for name changes', () => {
-    const { getByTestId } = render(<AddSchedForm />);
-    const schedName = getByTestId('schedule-name');
-    userEvent.type(schedName, 'My First Schedule');
-    expect(schedName.value).toBe('My First Schedule');
-  });
+describe("AddSchedForm tests", () => {
 
-  test('when I type a description, the state for description changes', () => {
-    const { getByTestId } = render(<AddSchedForm />);
-    const schedDescription = getByTestId('schedule-description');
-    userEvent.type(schedDescription, 'Classes I want');
-    expect(schedDescription.value).toBe('Classes I want');
-  });
+    test("renders without crashing", () => {
+        render(<AddSchedForm />);
+    });
 
-  test('when I choose a quarter, the state for quarter changes', () => {
-    const { getByLabelText } = render(<AddSchedForm />);
-    const schedQuarter = getByLabelText('Quarter');
-    userEvent.selectOptions(schedQuarter, '20211');
-    expect(schedQuarter.value).toBe('20211');
-  });
+    test("when I type a name, the state for name changes", () => {
+        const { getByTestId } = render(<AddSchedForm />);
+        const schedName = getByTestId("schedule-name")
+        userEvent.type(schedName, "My First Schedule");
+        expect(schedName.value).toBe("My First Schedule");
+    });
 
-  test('when I click submit, the right stuff happens', async () => {
-    const sampleReturnValue = {
-      sampleKey: 'sampleValue',
-    };
+    test("when I type a description, the state for description changes", () => {
+        const { getByTestId } = render(<AddSchedForm />);
+        const schedDescription = getByTestId("schedule-description")
+        userEvent.type(schedDescription, "Classes I want");
+        expect(schedDescription.value).toBe("Classes I want");
+    });
 
-    // Create spy functions (aka jest function, magic function)
-    // The function doesn't have any implementation unless
-    // we specify one.  But it does keep track of whether
-    // it was called, how many times it was called,
-    // and what it was passed.
+    test("when I choose a quarter, the state for quarter changes", () => {
+        const { getByTestId } = render(<AddSchedForm />);
+        const schedQuarter = getByTestId("schedule-quarter")
+        userEvent.type(schedQuarter, "F20");
+        expect(schedQuarter.value).toBe("F20");
+    });
 
-    const fetchJSONSpy = jest.fn();
-    const tokenSpy = 'token';
-    const successSpy = 'success';
-    const errorSpy = 'error';
+    test("when I click submit, the right stuff happens", async () => {
 
-    fetchJSONSpy.mockResolvedValue(sampleReturnValue);
+        const sampleReturnValue = {
+            "sampleKey": "sampleValue"
+        };
 
-    const { getByTestId, getByLabelText } = render(
-      <AddSchedForm
-        createSchedule={fetchJSONSpy}
-        getToken={tokenSpy}
-        onSuccess={successSpy}
-        onError={errorSpy}
-      />
-    );
+        // Create spy functions (aka jest function, magic function)
+        // The function doesn't have any implementation unless
+        // we specify one.  But it does keep track of whether
+        // it was called, how many times it was called,
+        // and what it was passed.
 
-    const expectedFields = {
-      name: 'MySchedule',
-      description: 'Classes',
-      quarter: '20211',
-      userId: '',
-    };
 
-    const schedName = getByTestId('schedule-name');
-    userEvent.type(schedName, 'MySchedule');
-    const schedDescription = getByTestId('schedule-description');
-    userEvent.type(schedDescription, 'Classes');
-    const schedQuarter = getByLabelText('Quarter');
-    userEvent.selectOptions(schedQuarter, '20211');
+        const fetchJSONSpy = jest.fn();
+        const tokenSpy = "token";
+        const successSpy = "success";
+        const errorSpy = "error";
 
-    const submitButton = getByTestId('schedule-submit');
-    userEvent.click(submitButton);
+        fetchJSONSpy.mockResolvedValue(sampleReturnValue);
 
-    // we need to be careful not to assert this expectation
-    // until all of the async promises are resolved
-    await waitFor(() => expect(fetchJSONSpy).toHaveBeenCalledTimes(1));
+        const { getByTestId } = render(
+            <AddSchedForm  createSchedule={fetchJSONSpy} getToken={tokenSpy} onSuccess={successSpy} onError={errorSpy} />
+        );
 
-    // assert that ourSpy was called with the right value
-    expect(fetchJSONSpy).toHaveBeenCalledWith(expectedFields);
-  });
+        const expectedFields = {
+            name: "MySchedule",
+            description: "Classes",
+            quarter: "F20",
+            userId: ""
+        };
+
+        const schedName = getByTestId("schedule-name")
+        userEvent.type(schedName, "MySchedule");
+        const schedDescription = getByTestId("schedule-description")
+        userEvent.type(schedDescription, "Classes");
+        const schedQuarter = getByTestId("schedule-quarter")
+        userEvent.type(schedQuarter, "F20");
+
+        const submitButton = getByTestId("schedule-submit");
+        userEvent.click(submitButton);
+
+        // we need to be careful not to assert this expectation
+        // until all of the async promises are resolved
+        await waitFor(() => expect(fetchJSONSpy).toHaveBeenCalledTimes(1));
+
+
+
+        // assert that ourSpy was called with the right value
+        expect(fetchJSONSpy).toHaveBeenCalledWith(expectedFields);
+    });
+
 });
