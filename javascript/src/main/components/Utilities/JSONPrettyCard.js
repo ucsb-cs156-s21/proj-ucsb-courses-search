df2a5823f5c6c0fcc83ee3d8c64406f1f42fd056
Sydney Lim <sydneylim@ucsb.edu>
2021-05-25 18:31:47 -0700

    sl - removed the course JSON response card completely

--- javascript/src/main/components/Utilities/JSONPrettyCard.js
+++ javascript/src/main/components/Utilities/JSONPrettyCard.js
@@ -6,30 +6,7 @@ import JSONPretty from "react-json-pretty";
 const JSONPrettyCard = (props) => {
   const [activeKey, setActiveKey] = useState(null);
 
-  return (
-    <Card>
-      <Card.Body>
-        <Accordion onSelect={(eventKey) => setActiveKey(eventKey)}>
-          <Accordion.Toggle
-            data-testid={`JSONPrettyPanel-${props.expression}-toggle`}
-            style={{ cursor: "pointer" }}
-            as={Card.Header}
-            variant="link"
-            eventKey="0"
-          >
-            Click here to {activeKey === "0" ? "hide" : "show" } the{" "}
-            {props.expression} response
-          </Accordion.Toggle>
-          <Accordion.Collapse
-            eventKey="0"
-            data-testid={`JSONPrettyPanel-${props.expression}-collapse`}
-          >
-            <JSONPretty data={props.value} />
-          </Accordion.Collapse>
-        </Accordion>
-      </Card.Body>
-    </Card>
-  );
+  return (null);
 };
 
 JSONPrettyCard.propTypes = {
