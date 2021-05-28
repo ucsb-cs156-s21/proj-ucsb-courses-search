51c485f91f52ff52862b50dfd890abf74a71b0cc
Alan Zhang <alan.zhang.052@gmail.com>
2021-05-27 21:52:15 -0700

    az - Added more code coverage

--- javascript/src/test/pages/Schedule/AddToSchedule.test.js
+++ javascript/src/test/pages/Schedule/AddToSchedule.test.js
@@ -2,9 +2,12 @@ import React from "react";
 import { render } from "@testing-library/react";
 import AddToSchedule from "main/pages/Schedule/AddToSchedule";
 import { useAuth0 } from "@auth0/auth0-react";
+import userEvent from "@testing-library/user-event";
 import useSWR from "swr";
+import { waitFor } from "@testing-library/react";
 import { useToasts } from "react-toast-notifications";
 import { useHistory, useParams } from "react-router-dom";
+import { fetchWithToken } from "main/utils/fetch";
 jest.mock("@auth0/auth0-react");
 jest.mock("swr");
 jest.mock("main/utils/fetch");
@@ -24,7 +27,7 @@ const schedules = [{
   "userId": "123456"
 }];
 
-describe("Schedule tests", () => {
+describe("Add to Schedule tests", () => {
 
   const getAccessTokenSilentlySpy = jest.fn();
   const mutateSpy = jest.fn();
@@ -61,6 +64,46 @@ describe("Schedule tests", () => {
       mutate: mutateSpy
     });
     render(<AddToSchedule />);
+  });
+
+  test("renders an error message when there is an error response in add", async () => {
+    useSWR.mockReturnValue({
+      data: schedules,
+      error: undefined,
+      mutate: mutateSpy
+    });
+    fetchWithToken.mockReturnValue({ error: "An error occured!" });
+    const { findByTestId } = render(<AddToSchedule />);
+    const addButton = await findByTestId("add-to-button-1");
+    userEvent.click(addButton);
+    await waitFor(() => expect(addToast).toHaveBeenCalledTimes(1));
+  });
+
+  test("test an error message appears when a response code is not 200 ok when adding", async () => {
+    useSWR.mockReturnValue({
+      data: schedules,
+      error: undefined,
+      mutate: mutateSpy
+    });
+    fetchWithToken.mockImplementation(() => { throw new Error("An error occured!") });
+    const { findByTestId } = render(<AddToSchedule />);
+    const addButton = await findByTestId("add-to-button-1");
+    userEvent.click(addButton);
+    await waitFor(() => expect(addToast).toHaveBeenCalledTimes(1));
+  });
+
+  test("test adding a schedule item successfully", async () => {
+    useSWR.mockReturnValue({
+      data: schedules,
+      error: undefined,
+      mutate: mutateSpy
+    });
+    fetchWithToken.mockReturnValue({});
+    const { findByTestId } = render(<AddToSchedule />);
+    const addButton = await findByTestId("add-to-button-1");
+    userEvent.click(addButton);
+    await waitFor(() => expect(mutateSpy).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(addToast).toHaveBeenCalledTimes(1));
   });
   
 });
