b574252ed8a24f8a856e95154dddd97d2f80ee7c
Evan Yip <etyip@ucsb.edu>
2021-05-27 17:04:54 -0700

    ey - removed useEffect from addSchedForm

--- javascript/src/main/components/Schedule/AddSchedForm.js
+++ javascript/src/main/components/Schedule/AddSchedForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import SelectQuarter from 'main/components/BasicCourseSearch/SelectQuarter';
 import { quarterRange } from 'main/utils/quarterUtilities';
